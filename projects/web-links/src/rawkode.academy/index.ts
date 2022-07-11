b43850c30382d5840d167e3510f00fd8641f6dc3
David Flanagan <david@rawkode.dev>
2022-07-11 19:48:22 +0100

    chore: use rawkode academy team instead

--- projects/web-links/src/rawkode.academy/index.ts
+++ projects/web-links/src/rawkode.academy/index.ts
@@ -4,10 +4,10 @@ const redirects: Domain = {
   defaultRedirect: "https://youtube.com/c/rawkode",
   redirects: {
     "shareyourproject": {
-      to: "https://savvycal.com/rawkode/be-on-rawkode-live",
+      to: "https://savvycal.com/RawkodeAcademy/share-your-project",
     },
     "share-your-project": {
-      to: "https://savvycal.com/rawkode/be-on-rawkode-live",
+      to: "https://savvycal.com/RawkodeAcademy/share-your-project",
     },
     hackdays: {
       to: "https://github.com/rawkode-academy/hackdays",
