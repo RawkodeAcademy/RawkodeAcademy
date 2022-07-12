87ec21f2da0af116880c389da7c48f0ab5ea892a
David Flanagan <david@rawkode.dev>
2022-07-12 12:42:38 +0100

    feat: add Teleport and Metal vanity links

--- projects/web-links/src/rawkode.academy/index.ts
+++ projects/web-links/src/rawkode.academy/index.ts
@@ -3,6 +3,12 @@ import { Domain } from "../types";
 const redirects: Domain = {
   defaultRedirect: "https://youtube.com/c/rawkode",
   redirects: {
+    "metal": {
+      to: "https://eqix.it/3tKSpuh",
+    },
+    "teleport": {
+      to: "https://goteleport.com/?utm_campaign=eg&utm_medium=partner&utm_source=rawkode",
+    },
     "shareyourproject": {
       to: "https://savvycal.com/RawkodeAcademy/share-your-project",
     },
