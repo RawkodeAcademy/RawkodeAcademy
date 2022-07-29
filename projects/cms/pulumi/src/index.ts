054f5ab39370129abe16a76459aa2cd4b3739ca4
David Flanagan <david@rawkode.dev>
2022-07-29 23:40:59 +0100

    chore: add test cm

--- projects/cms/pulumi/src/index.ts
+++ projects/cms/pulumi/src/index.ts
@@ -1,4 +1,5 @@
 import * as random from "@pulumi/random";
+import * as kubernetes from "@pulumi/kubernetes";
 import * as atlas from "@pulumi/mongodbatlas";
 import * as doppler from "@pulumiverse/doppler";
 
@@ -6,6 +7,12 @@ const orgId = process.env.MONGODB_ATLAS_ORG_ID!;
 
 const project = new atlas.Project("cms", {
   orgId,
+});
+
+const configmap = new kubernetes.core.v1.ConfigMap("test", {
+  data: {
+    abc: "123",
+  },
 });
 
 const database = new atlas.ServerlessInstance("cms", {
