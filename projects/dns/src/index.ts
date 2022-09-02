0215a6611953f3ae06cc29f4775769675e6a669d
David Flanagan <david@rawkode.dev>
2022-09-02 10:54:32 +0100

    chore: add backend to Pulumi.yaml to avoid logging in/out

--- projects/dns/src/index.ts
+++ projects/dns/src/index.ts
@@ -31,7 +31,7 @@ const reconcileDomain = (domain: Domain) => {
         },
       ],
       nonExistence: ManagedZoneDnsSecConfigNonExistence.Nsec3,
-      state: ManagedZoneDnsSecConfigState.Transfer,
+      state: ManagedZoneDnsSecConfigState.On,
     },
   });
 
