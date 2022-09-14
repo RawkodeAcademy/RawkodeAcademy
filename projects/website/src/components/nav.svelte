9d70f721388f7e4baf72dc92d5c045f84f559334
David Flanagan <david@rawkode.dev>
2022-09-14 18:20:59 +0100

    chore: hide command k bar

--- projects/website/src/components/nav.svelte
+++ projects/website/src/components/nav.svelte
@@ -36,28 +36,4 @@
       />
     </svg>
   </button>
-
-  <div class="flex-1 px-4 flex justify-between">
-    <div class="flex-1 flex">
-      <div class="w-full flex text-center md:ml-0">
-        <div
-          class="relative w-full text-gray-400 focus-within:text-gray-600"
-          on:click={showCommandPalette}
-        >
-          <div
-            class="absolute w-full inset-y-0 left-0 flex items-center pointer-events-none"
-          >
-            <input
-              id="search-field"
-              class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-              placeholder="Click to open the command palette ..."
-              type="search"
-              name="search"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- <div class="ml-4 flex items-center md:ml-6">Right</div> -->
-  </div>
 </div>
