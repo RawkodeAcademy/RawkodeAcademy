e7cb4fa14d4d789f687271cbf664fa9a48b34292
David Flanagan <david@rawkode.dev>
2022-11-08 18:08:42 +0000

    fix: use correct package for kubectl helper

--- dagger/pulumi/scripts/up.sh
+++ dagger/pulumi/scripts/up.sh
@@ -2,11 +2,10 @@
 set -xeo pipefail
 
 if test -v GCLOUD_GKE; then
-  apt update && apt install --yes curl gnupg python3
+  apt update && apt install --yes curl gnupg 
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-  apt update && apt install --yes google-cloud-cli
-  gcloud components install gke-gcloud-auth-plugin
+  apt update && apt install --yes google-cloud-sdk-gke-gcloud-auth-plugin
 fi
 
 if test -v PULUMI_CONFIG_PASSPHRASE || test -v PULUMI_CONFIG_PASSPHRASE_FILE; then
