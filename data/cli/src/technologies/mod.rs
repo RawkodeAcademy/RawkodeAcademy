768c6d30c0c4880d80b6b59ce908ecd1d8b69bc2
David Flanagan <david@rawkode.dev>
2022-12-06 00:23:21 +0000

    feat: simpler Rust approach to data validation

--- data/cli/src/technologies/mod.rs
+++ data/cli/src/technologies/mod.rs
@@ -1,59 +1,33 @@
-use sea_orm::entity::prelude::*;
+use crate::loader::ID;
+use serde::Deserialize;
+use validator::Validate;
 
-mod commands;
-pub use commands::create;
-
-#[derive(Copy, Clone, Default, Debug, DeriveEntity)]
-pub struct Entity;
-
-impl EntityName for Entity {
-    fn schema_name(&self) -> Option<&str> {
-        Some("public")
-    }
-
-    fn table_name(&self) -> &str {
-        "technologies"
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, DeriveModel, DeriveActiveModel)]
-#[sea_orm(table_name = "technology")]
-pub struct Model {
+#[derive(Clone, Debug, Deserialize, Validate)]
+pub struct Technology {
+    #[validate(length(min = 1, max = 255))]
     pub name: String,
     pub description: String,
+
+    #[validate(url)]
+    pub code_repository: Option<String>,
+
+    #[validate(url)]
+    pub website: Option<String>,
+
+    #[validate(url)]
+    pub documentation: Option<String>,
+
+    #[validate(length(min = 4, max = 15))]
+    #[serde(alias = "twitter")]
+    pub twitter_handle: Option<String>,
+
+    #[validate(length(min = 3, max = 30))]
+    #[serde(alias = "youtube")]
+    pub youtube_handle: Option<String>,
 }
 
-#[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
-pub enum Column {
-    Name,
-    Description,
-}
-
-#[derive(Copy, Clone, Debug, EnumIter, DerivePrimaryKey)]
-pub enum PrimaryKey {
-    Name,
-}
-
-impl PrimaryKeyTrait for PrimaryKey {
-    type ValueType = String;
-
-    fn auto_increment() -> bool {
-        false
+impl ID for Technology {
+    fn id(&self) -> String {
+        self.name.clone()
     }
 }
-
-#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
-
-impl ColumnTrait for Column {
-    type EntityName = Entity;
-
-    fn def(&self) -> ColumnDef {
-        match self {
-            Self::Name => ColumnType::String(Some(64)).def(),
-            Self::Description => ColumnType::Text.def(),
-        }
-    }
-}
-
-impl ActiveModelBehavior for ActiveModel {}
