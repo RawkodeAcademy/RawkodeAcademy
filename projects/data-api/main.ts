919f5bd2684e8844730d4c7eda652b7343309664
David Flanagan <david@rawkode.dev>
2022-12-12 17:54:14 +0000

    experiment: playing with SurrealDB

--- projects/data-api/main.ts
+++ projects/data-api/main.ts
@@ -1,47 +1,79 @@
 import Surreal from "https://deno.land/x/surrealdb/mod.ts";
+import { parse as parseYaml } from "https://deno.land/std@0.82.0/encoding/yaml.ts";
 
-const db = new Surreal("http://127.0.0.1:8000/rpc");
+const surrealDb = new Surreal(
+    Deno.env.get("SURREALDB_HOST") || "http://127.0.0.1:8000/rpc"
+);
+const surrealDbUser = Deno.env.get("SURREALDB_USER") || "root";
+const surrealDbPass = Deno.env.get("SURREALDB_PASS") || "root";
+
+const surrealDbNamespace = "rawkode";
+const surrealDbDatabase = "data";
+
+const dataDir = Deno.env.get("DATA_DIR") || "../../data";
+
+const convertPerson = (person: any): any => {
+    return {
+        ...person,
+        github: {
+            handle: person.github,
+        },
+    };
+};
 
 async function main() {
     try {
-        await db.signin({
-            user: "root",
-            pass: "root",
+        await surrealDb.signin({
+            user: surrealDbUser,
+            pass: surrealDbPass,
         });
 
-        await db.use("rawkode.academy", "data");
-
-        console.log("Done");
-        // let created = await db.create("person", {
-        //   title: "Founder & CEO",
-        //   name: {
-        //     first: "Tobie",
-        //     last: "Morgan Hitchcock",
-        //   },
-        //   marketing: true,
-        //   identifier: Math.random().toString(36).substr(2, 10),
-        // });
-
-        // // Update a person record with a specific id
-        // let updated = await db.change("person:jaime", {
-        //   marketing: true,
-        // });
-
-        // // Select all people records
-        // let people = await db.select("person");
-
-        // // Perform a custom advanced query
-        // let groups = await db.query(
-        //   "SELECT marketing, count() FROM type::table($tb) GROUP BY marketing",
-        //   {
-        //     tb: "person",
-        //   }
-        // );
+        await surrealDb.query(`DEFINE NAMESPACE ${surrealDbNamespace}`);
+        await surrealDb.query(
+            `USE NAMESPACE ${surrealDbNamespace} ; DEFINE DATABASE ${surrealDbDatabase}`
+        );
+        await surrealDb.use(surrealDbNamespace, surrealDbDatabase);
     } catch (e) {
         console.error("Failed to connect to database: ", e);
-    } finally {
-        db.close();
+        return;
     }
+
+    const schema = await Deno.readTextFile("./schema.surql");
+
+    await (
+        await surrealDb.query(schema)
+    ).forEach((result) => {
+        if (result.error) {
+            console.error("Failed to create schema: ", result.error);
+            return;
+        }
+    });
+
+    console.log("Schema Created");
+
+    const dataTypes = ["people"];
+    for (const dataType of dataTypes) {
+        for (const dirEntry of Deno.readDirSync(`${dataDir}/${dataType}`)) {
+            if (dirEntry.isFile && dirEntry.name.endsWith(".yaml")) {
+                const data = parseYaml(
+                    await Deno.readTextFile(
+                        `${dataDir}/${dataType}/${dirEntry.name}`
+                    )
+                ) as Object;
+
+                console.debug(convertPerson(data));
+
+                const result = await surrealDb.create(
+                    `${dataType}:${dirEntry.name.replace(".yaml", "")}`,
+                    convertPerson(data)
+                );
+
+                console.debug(result);
+            }
+        }
+    }
+
+    surrealDb.close();
 }
 
 await main();
