82425bb401871afc972459f38d95808125ea9a3a
David Flanagan <david@rawkode.dev>
2022-12-12 20:23:45 +0000

    feat: add pull-request logic

--- dagger.ts
+++ dagger.ts
@@ -1,10 +1,21 @@
 #!/usr/bin/env zx
 import Client, { connect } from "@dagger.io/dagger";
 import * as website from "./projects/website/dagger.js";
+import { PullRequest } from "./dagger/github/index.js";
 
 connect(
   async (client: Client) => {
-    await website.deploy(client);
+    const pullRequestRef = await client
+      .host()
+      .envVariable("GITHUB_HEAD_REF")
+      .value();
+
+    const pullRequest: PullRequest = {
+      isIt: pullRequestRef !== undefined,
+      ref: pullRequestRef,
+    };
+
+    await website.deploy(client, pullRequest);
   }
   // Until there's a programmatic secret API, do not merge a commit
   // with this enabled. It prints all secrets to the web viewer.
--- projects/website/dagger.ts
+++ projects/website/dagger.ts
@@ -2,8 +2,10 @@ import Client, { connect } from "@dagger.io/dagger";
 import { v4 as uuidv4 } from "uuid";
 import { getSourceDir } from "../../dagger/utils/index.js";
 import { getSecrets } from "../../dagger/doppler/index.js";
+import { PullRequest } from "../../dagger/github/index.js";
+import { Container } from "@dagger.io/dagger/dist/api/client.gen.js";
 
-export const deploy = async (client: Client) => {
+export const deploy = async (client: Client, pullRequest: PullRequest) => {
   const uuid = uuidv4();
 
   const sourcePath = getSourceDir(import.meta.url);
@@ -51,12 +53,23 @@ export const deploy = async (client: Client) => {
     .withWorkdir("/app")
     .withExec(["npm", "run", "build"]);
 
-  const deploy = await build
-    .withNewFile("/gcloud.json", {
-      contents: secrets["FIREBASE_TOKEN"].computed,
-    })
-    .withEnvVariable("GOOGLE_APPLICATION_CREDENTIALS", "/gcloud.json")
-    .withExec(["npm", "run", "firebase:deploy"]);
+  let result: Container;
 
-  console.log(await deploy.stdout());
+  if (pullRequest.isIt === true) {
+    result = await build
+      .withNewFile("/gcloud.json", {
+        contents: secrets["FIREBASE_TOKEN"].computed,
+      })
+      .withEnvVariable("GOOGLE_APPLICATION_CREDENTIALS", "/gcloud.json")
+      .withExec(["npx", "firebase", "hosting:channel:deploy", pullRequest.ref]);
+  } else {
+    let result = await build
+      .withNewFile("/gcloud.json", {
+        contents: secrets["FIREBASE_TOKEN"].computed,
+      })
+      .withEnvVariable("GOOGLE_APPLICATION_CREDENTIALS", "/gcloud.json")
+      .withExec(["npx", "firebase", "deploy", "--only=hosting"]);
+  }
+
+  console.log(await result.stdout());
 };
