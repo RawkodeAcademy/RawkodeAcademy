510d558d597ecb9916a954ff3c2db3b341467736
David Flanagan <david@rawkode.dev>
2022-12-12 19:52:07 +0000

    fix: hide secrets by omitting log viewer

--- dagger.ts
+++ dagger.ts
@@ -5,8 +5,10 @@ import * as website from "./projects/website/dagger.js";
 connect(
   async (client: Client) => {
     await website.deploy(client);
-  },
-  {
-    LogOutput: process.stdout,
   }
+  // Until there's a programmatic secret API, do not merge a commit
+  // with this enabled. It prints all secrets to the web viewer.
+  // {
+  //   LogOutput: process.stdout,
+  // }
 );
