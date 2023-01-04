c00a275df0afa56f2cb9eafaa330f0c068d7de5d
David Flanagan <david@rawkode.dev>
2023-01-04 11:28:15 +0000

    fix: use standard creation
    Pulumi CustomResource looks for a provider that doesn't exist. It's a bug.
    
    Avoid for now

--- projects/rawkode.studio/deploy/src/index.ts
+++ projects/rawkode.studio/deploy/src/index.ts
@@ -1,2 +1,17 @@
-import { Temporal } from "./temporal";
-const temporal = new Temporal();
+import * as kubernetes from "@pulumi/kubernetes";
+
+new kubernetes.apiextensions.CustomResource("postgresql", {
+	apiVersion: "postgresql.cnpg.io/v1",
+	kind: "Cluster",
+	metadata: {
+		name: "temporal-postgresql",
+	},
+	spec: {
+		instances: 3,
+		imageName: "ghcr.io/cloudnative-pg/postgresql:13.4",
+		primaryUpdateStrategy: "unsupervised",
+		storage: {
+			size: "10Gi",
+		},
+	},
+});
