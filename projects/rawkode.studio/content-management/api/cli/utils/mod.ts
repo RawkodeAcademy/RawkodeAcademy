c3df34fd37a16ce4413bf19403b705aaafbd2f04
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2023-01-05 23:33:47 +0100

    [cleanup] get rid of TS based CLI

--- projects/rawkode.studio/content-management/api/cli/utils/mod.ts
+++
@@ -1,3 +0,0 @@
-export const slugify = (text: string): string => {
-    return text.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase().replace(/["']+/gi, "").replace(/[^a-z0-9]+/gi, "-").replace(/^-+|-+$/g, "");
-}
