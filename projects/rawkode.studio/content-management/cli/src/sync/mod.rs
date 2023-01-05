82d3e20075e18088b79686ebc51e2e0e64790cd7
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2023-01-05 23:11:47 +0100

    [nit] naming

--- projects/rawkode.studio/content-management/cli/src/sync/mod.rs
+++ projects/rawkode.studio/content-management/cli/src/sync/mod.rs
@@ -1,4 +1,4 @@
-use crate::utils::{build_inmem_database, find_hcl_files};
+use crate::utils::{build_in_memory_database, find_hcl_files};
 use miette::{IntoDiagnostic, Result};
 use sqlx::postgres::PgPoolOptions;
 use std::path::PathBuf;
@@ -7,7 +7,7 @@ pub async fn command(path: PathBuf, _apply: bool) -> Result<()> {
     let files = find_hcl_files(path)?;
 
     println!("Syncing {} files", files.len());
-    let database = build_inmem_database(files)?;
+    let database = build_in_memory_database(files)?;
 
     let connection_string = std::env::var("DATABASE_DSN")
         .unwrap_or_else(|_| "postgres://academy:academy@localhost:5432/academy".to_string());
