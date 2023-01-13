66ee1550b5b0fdfc09a1876af0096c9efc655e9e
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2023-01-13 18:02:28 +0100

    [feat] remove stale rows (#35)
    * [feat] remove stale rows
    
    Co-authored-by: David Flanagan <david@rawkode.dev>

--- projects/rawkode.studio/content-management/api/migrations/data/1671035042735_initial/down.sql
+++ projects/rawkode.studio/content-management/api/migrations/data/1671035042735_initial/down.sql
@@ -1,15 +1,20 @@
 DROP VIEW IF EXISTS "episode_guests_view";
 DROP VIEW IF EXISTS "guest_episodes_view";
-
-DROP TABLE IF EXISTS "episode_guests";
-DROP TABLE IF EXISTS "episodes";
-
-DROP TYPE "chapter";
-
 DROP VIEW IF EXISTS "show_hosts_view";
 DROP VIEW IF EXISTS "host_shows_view";
+DROP VIEW IF EXISTS "technology_episodes_view";
+DROP VIEW IF EXISTS "episode_technologies_view";
 
+DROP TABLE IF EXISTS "episode_guests";
 DROP TABLE IF EXISTS "show_hosts";
-DROP TABLE IF EXISTS "shows";
+
+DROP TABLE IF EXISTS "episode_technologies";
+
+DROP TABLE IF EXISTS "episodes";
+
 DROP TABLE IF EXISTS "people";
 DROP TABLE IF EXISTS "technologies";
+DROP TABLE IF EXISTS "shows";
+
+
+DROP TYPE "chapter";
