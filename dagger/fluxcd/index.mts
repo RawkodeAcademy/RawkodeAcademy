a4d56cb6a73567620bad24f1f5aca2660d70af81
David Flanagan <david@rawkode.dev>
2023-01-24 00:13:25 +0000

    chore: couple of cjs/mts fixes

---
+++ dagger/fluxcd/index.mts
@@ -0,0 +1,38 @@
+import Client from "@dagger.io/dagger";
+import { Directory } from "@dagger.io/dagger/dist/api/client.gen";
+import "zx/globals";
+
+const VERSION = "v0.38.2";
+
+interface Config {
+	githubToken: string;
+	artifactName: string;
+	path: Directory;
+}
+
+export const pushArtifact = async (client: Client, config: Config) => {
+	const source = await (await $`git remote get-url origin`).toString().trim();
+	const branch = await (await $`git branch --show-current`).toString().trim();
+	const revision = await (await $`git rev-parse HEAD`).toString().trim();
+
+	const fluxCli = client
+		.container()
+		.from(`ghcr.io/fluxcd/flux-cli:${VERSION}`)
+		.withUser("root")
+		.withMountedDirectory("/app", config.path)
+		.withEntrypoint(["flux"]);
+
+	const result = await fluxCli.withExec([
+		"push",
+		"artifact",
+		`--creds=rawkode:${config.githubToken}`,
+		`oci://ghcr.io/rawkodeacademy/${config.artifactName}:latest`,
+		"--path=/app",
+		`--source=${source}`,
+		`--revision=${branch}/${revision}`,
+	]);
+
+	if ((await result.exitCode()) !== 0) {
+		throw new Error(await result.stderr());
+	}
+};
--- projects/rawkode.studio/dagger/deploy.ts
+++ projects/rawkode.studio/dagger/deploy.ts
@@ -1,33 +1,33 @@
 import Client from "@dagger.io/dagger";
-import { DaggerCommand, SecretApi } from "@rawkode.academy/dagger/index.js";
-import { getSourceDir } from "@rawkode.academy/dagger/utils/index.js";
-import { pushArtifact } from "@rawkode.academy/dagger/fluxcd/index.js";
+import { DaggerCommand } from "@rawkode.academy/dagger/index.mjs";
+import { resolveSecret } from "@rawkode.academy/dagger/secrets/index.mjs";
+import { getSourceDir } from "@rawkode.academy/dagger/utils/index.mjs";
+import { pushArtifact } from "@rawkode.academy/dagger/fluxcd/index.mjs";
 
-export const deploy = async (
-  client: Client,
-  getSecrets: SecretApi
-): Promise<boolean> => {
-  const secrets = await getSecrets("studio", "production", ["GITHUB_TOKEN"]);
+export const execute = async (client: Client): Promise<boolean> => {
+	const githubToken = await resolveSecret({
+		vault: "sa.rawkode.studio",
+		item: "GitHub",
+		key: "password",
+	});
 
-  const sourcePath = getSourceDir(`${import.meta.url}/..`);
-  const sourceDirectory = await client
-    .host()
-    .directory(`${sourcePath}/deploy`, {
-      exclude: [".git", ".pnpm-store", "dagger", "dagger.ts", "node_modules"],
-    });
+	const sourcePath = getSourceDir(`${import.meta.url}/..`);
+	const sourceDirectory = client.host().directory(`${sourcePath}/deploy`, {
+		exclude: [".git", ".pnpm-store", "dagger", "dagger.ts", "node_modules"],
+	});
 
-  await pushArtifact(client, {
-    artifactName: "studio-deploy",
-    githubToken: secrets["GITHUB_TOKEN"],
-    path: sourceDirectory,
-  });
+	await pushArtifact(client, {
+		artifactName: "studio-deploy",
+		githubToken: githubToken,
+		path: sourceDirectory,
+	});
 
-  return true;
+	return true;
 };
 
 const command: DaggerCommand = {
-  name: "deploy",
-  description: "Deploy",
-  execute: deploy,
+	name: "deploy",
+	description: "Push Deployment Manifests to OCI Registry",
+	execute,
 };
 export default command;
