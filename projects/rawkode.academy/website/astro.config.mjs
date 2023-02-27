d3ef5a207051379ce3b278fa3e680126396f871f
David Flanagan <david@rawkode.dev>
2023-02-27 16:51:24 +0000

    chore: collaborate with hubspot

--- projects/rawkode.academy/website/astro.config.mjs
+++ projects/rawkode.academy/website/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
@@ -9,6 +10,11 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [
     mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     react(),
     sitemap(),
     svelte(),
