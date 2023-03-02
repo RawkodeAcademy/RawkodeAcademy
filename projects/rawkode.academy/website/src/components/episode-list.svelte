bf0ff94e7532c522d6a503ed0ff83c3e482a857f
Ally Graham <ally@allygraham.co.uk>
2023-03-02 22:10:19 +0000

    feat: got the load more button working

--- projects/rawkode.academy/website/src/components/episode-list.svelte
+++ projects/rawkode.academy/website/src/components/episode-list.svelte
@@ -1,22 +1,47 @@
 <script>
+  import InfiniteScroll from "@components/infinite-scroll.svelte";
   export let list;
 
-  let page = 1;
+  let page = 0;
   let size = 14;
+  let episodes = [];
+
+  $:episodes = [
+    ...episodes,
+    ...list.slice(size * page, (page + 1) * size),
+  ];
+
+  const handleClick = () => {
+    page++;
+  };
 </script>
 
 <ul class="episodes__list">
-  {#each list.slice(0, (page * size)) as episode}
-  <li>
-    <a href={`https://www.youtube.com/watch?v=${episode.youtube_id}`} target="_blank" class="episode__card" rel="noreferrer">
-      <div class="episode__content">
-        <p>{episode.title}</p>
-        <img src={`https://img.youtube.com/vi/${episode.youtube_id}/hqdefault.jpg`} alt={episode.title} />
-      </div>
-    </a>
-  </li>
-{/each}
+  {#each episodes as episode}
+    <li>
+      <a href={`https://www.youtube.com/watch?v=${episode.youtube_id}`} target="_blank" class="episode__card" rel="noreferrer">
+        <div class="episode__content">
+          <p>{episode.title}</p>
+          <img src={`https://img.youtube.com/vi/${episode.youtube_id}/hqdefault.jpg`} alt={episode.title} />
+        </div>
+      </a>
+    </li>
+  {/each}
+  <InfiniteScroll
+    client:load
+    hasMore={episodes.length < list.length}
+    threshold={10}
+    on:loadMore={() => handleClick()} />
 </ul>
-{#if (page * size) < list.length}
-<button on:click={() => {page++;}} type="button" class="button--load-more">Load more</button>
+{#if episodes.length < list.length}
+<button
+    on:click={handleClick}
+    class="button--load-more"
+    type="button"
+  >
+    Load more
+  </button>
+{/if}
+{#if episodes.length === list.length && page > 0}
+  <p class="end-of-episodes">End of episodes</p>
 {/if}
