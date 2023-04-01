0b65754513512e2720552ec1070e1c9cc36b1cf8
Ally Graham <ally@allygraham.co.uk>
2023-04-01 18:09:25 +0100

    feat: updates to the homepage to add in counters

---
+++ projects/rawkode.academy/website/src/components/counter.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Counter from 'svelte-counter';
+  export let counters = {
+    'views'	: 350,
+    'time' : 41,
+  };
+</script>
+
+<Counter values={counters} duration="5000" random="false" minspeed="100" let:counterResult>
+  <div class="counter__container"><p>Video views</p><p>{counterResult.views}k+</p></div>
+  <div class="counter__container"><p>Watch time <span>(hours)</span></p><p>{counterResult.time}k+</p></div>
+</Counter>
