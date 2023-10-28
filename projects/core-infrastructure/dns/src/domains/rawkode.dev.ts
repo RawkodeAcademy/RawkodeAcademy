c23c90454f6370d66c0a58fffad6a580a0e5d301
David Flanagan <david@rawkode.dev>
2023-10-28 21:25:30 +0100

    fix: dkim keys

--- projects/core-infrastructure/dns/src/domains/rawkode.dev.ts
+++ projects/core-infrastructure/dns/src/domains/rawkode.dev.ts
@@ -10,7 +10,7 @@ export default (scope: Construct): ManagedDomain => {
 
 	managedDomain
     .enableGSuite({
-      domainKey: "",
+      domainKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhiWae1/qb61lZzpUW8pnwS+97DkHVdjz42VV27sGvA1NRdXNh1qN2Yh7p8na6riahggJO+YLKuPSK35NUPqceDZCvtv0jdnO3drifcmL8ZeI8xcn421H19UrUajV8Qow9ZnErLMlHlbr7zBl6aJthUYtGEACTsWUwTY3mCeM3r5iEDJBPpTu8GGkGoBktJJx5GoadQ7T821J1c4LqXU9ZF8Gqtq0rL3QUf4aQxsHbJeJQLzrDmpDV6xHTJOBk+JNRYD2DZWv0LxBD3eJGa40gvqfsx5SeD6XlSX5JuzHJjiCUBdjL4CMtdEt0lQWetF22TQ9wCz8hwhwzv7qGW099wIDAQAB",
       spfIncludes: [],
     }).addTextRecord(
 			"google-site-verification",
