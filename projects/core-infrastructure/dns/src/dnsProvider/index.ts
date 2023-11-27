ecea485ca18183d289fb7d6912e775cbed588344
David Flanagan <david@rawkode.dev>
2023-11-27 20:16:17 +0000

    fix: format code

--- projects/core-infrastructure/dns/src/dnsProvider/index.ts
+++ projects/core-infrastructure/dns/src/dnsProvider/index.ts
@@ -1,6 +1,6 @@
 import {
-	PageRule,
-	PageRuleActions,
+  PageRule,
+  PageRuleActions,
 } from "@generatedProviders/cloudflare/page-rule";
 import { Record } from "@generatedProviders/cloudflare/record";
 import { Zone } from "@generatedProviders/cloudflare/zone";
@@ -10,20 +10,20 @@ import { Nameservers } from "@generatedProviders/gandi/nameservers";
 import { Construct } from "constructs";
 
 enum Email {
-	Unset,
-	Configured,
-	Discouraged,
+  Unset,
+  Configured,
+  Discouraged,
 }
 
 export enum Registrar {
-	Cloudflare,
-	Gandi,
-	Dynadot,
+  Cloudflare,
+  Gandi,
+  Dynadot,
 }
 
 interface GSuiteConfig {
-	domainKey: string;
-	spfIncludes: string[];
+  domainKey: string;
+  spfIncludes: string[];
 }
 
 interface BumpEmailConfig {
@@ -31,163 +31,168 @@ interface BumpEmailConfig {
 }
 
 export class ManagedDomain extends Construct {
-	private readonly registrar: Registrar;
-	private readonly cloudflareZone: Zone;
-	private email: Email = Email.Unset;
+  private readonly registrar: Registrar;
+  private readonly cloudflareZone: Zone;
+  private email: Email = Email.Unset;
 
-	constructor(scope: Construct, zone: string, registrar: Registrar) {
-		super(scope, zone);
+  constructor(scope: Construct, zone: string, registrar: Registrar) {
+    super(scope, zone);
 
-		this.registrar = registrar;
+    this.registrar = registrar;
 
-		this.cloudflareZone = new Zone(this, "zone", {
-			zone,
-			accountId: process.env.CLOUDFLARE_ACCOUNT_ID || "",
-			type: "full",
-			plan: "free",
-			lifecycle: {
-				preventDestroy: true,
-			},
-		});
+    this.cloudflareZone = new Zone(this, "zone", {
+      zone,
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID || "",
+      type: "full",
+      plan: "free",
+      lifecycle: {
+        preventDestroy: true,
+      },
+    });
 
-		new ZoneSettingsOverride(this, "zone-settings", {
-			zoneId: this.cloudflareZone.id,
-			settings: {
-				cnameFlattening: "flatten_at_root",
-				alwaysUseHttps: "on",
-				automaticHttpsRewrites: "on",
-				opportunisticEncryption: "on",
-				minTlsVersion: "1.2",
-				tls13: "on",
-				ssl: "flexible",
-				universalSsl: "on",
-			},
-		});
+    new ZoneSettingsOverride(this, "zone-settings", {
+      zoneId: this.cloudflareZone.id,
+      settings: {
+        cnameFlattening: "flatten_at_root",
+        alwaysUseHttps: "on",
+        automaticHttpsRewrites: "on",
+        opportunisticEncryption: "on",
+        minTlsVersion: "1.2",
+        tls13: "on",
+        ssl: "flexible",
+        universalSsl: "on",
+      },
+    });
 
-		switch (registrar) {
-			case Registrar.Cloudflare:
-				new ZoneDnssec(this, "dnssec", {
-					zoneId: this.cloudflareZone.id,
-				});
-				break;
+    switch (registrar) {
+      case Registrar.Cloudflare:
+        new ZoneDnssec(this, "dnssec", {
+          zoneId: this.cloudflareZone.id,
+        });
+        break;
 
-			case Registrar.Gandi:
-				new Nameservers(this, "nameservers", {
-					domain: zone,
-					nameservers: this.cloudflareZone.nameServers,
-				});
-				break;
-		}
-	}
+      case Registrar.Gandi:
+        new Nameservers(this, "nameservers", {
+          domain: zone,
+          nameservers: this.cloudflareZone.nameServers,
+        });
+        break;
+    }
+  }
 
-	addPageRule(
-		id: string,
-		target: string,
-		actions: PageRuleActions,
-	): ManagedDomain {
-		if (this.registrar !== Registrar.Cloudflare) {
-			throw new Error("Page rules are only supported for Cloudflare domains");
-		}
+  addPageRule(
+    id: string,
+    target: string,
+    actions: PageRuleActions,
+  ): ManagedDomain {
+    if (this.registrar !== Registrar.Cloudflare) {
+      throw new Error("Page rules are only supported for Cloudflare domains");
+    }
 
-		new PageRule(this, id, {
-			zoneId: this.cloudflareZone.id,
-			target,
-			actions,
-		});
-
-		return this;
-	}
-
-	addARecord(id: string, name: string, value: string): ManagedDomain {
-		new Record(this, id, {
-			zoneId: this.cloudflareZone.id,
-			type: "A",
-			ttl: 300,
-			name,
-			value,
-			comment: "Managed by Terraform",
-		});
+    new PageRule(this, id, {
+      zoneId: this.cloudflareZone.id,
+      target,
+      actions,
+    });
 
     return this;
   }
 
-  addCNameRecord(id: string, name: string, value: string, proxied?: boolean): ManagedDomain {
-		new Record(this, id, {
-			zoneId: this.cloudflareZone.id,
-			type: "CNAME",
-			ttl: 300,
-			name,
-			value,
+  addARecord(id: string, name: string, value: string): ManagedDomain {
+    new Record(this, id, {
+      zoneId: this.cloudflareZone.id,
+      type: "A",
+      ttl: 300,
+      name,
+      value,
+      comment: "Managed by Terraform",
+    });
+
+    return this;
+  }
+
+  addCNameRecord(
+    id: string,
+    name: string,
+    value: string,
+    proxied?: boolean,
+  ): ManagedDomain {
+    new Record(this, id, {
+      zoneId: this.cloudflareZone.id,
+      type: "CNAME",
+      ttl: 300,
+      name,
+      value,
       proxied: proxied || false,
-			comment: "Managed by Terraform",
-		});
+      comment: "Managed by Terraform",
+    });
 
-		return this;
-	}
+    return this;
+  }
 
-	addTextRecord(id: string, name: string, value: string): ManagedDomain {
-		new Record(this, id, {
-			zoneId: this.cloudflareZone.id,
-			type: "TXT",
-			ttl: 300,
-			name,
-			value,
-			comment: "Managed by Terraform",
-		});
+  addTextRecord(id: string, name: string, value: string): ManagedDomain {
+    new Record(this, id, {
+      zoneId: this.cloudflareZone.id,
+      type: "TXT",
+      ttl: 300,
+      name,
+      value,
+      comment: "Managed by Terraform",
+    });
 
-		return this;
-	}
+    return this;
+  }
 
-	discourageEmail(): ManagedDomain {
-		switch (this.email) {
-			case Email.Discouraged:
-				return this;
+  discourageEmail(): ManagedDomain {
+    switch (this.email) {
+      case Email.Discouraged:
+        return this;
 
-			case Email.Unset:
-				this.email = Email.Discouraged;
-        this.addTextRecord("discourage-email", "@", 'v=spf1 ~all');
-				return this;
+      case Email.Unset:
+        this.email = Email.Discouraged;
+        this.addTextRecord("discourage-email", "@", "v=spf1 ~all");
+        return this;
 
-			case Email.Configured:
-				throw new Error(
-					"Attempting to discourage email, but email has already been enabled",
-				);
-		}
-	}
+      case Email.Configured:
+        throw new Error(
+          "Attempting to discourage email, but email has already been enabled",
+        );
+    }
+  }
 
   enableFastmail(): ManagedDomain {
-		new Record(this, "mx1", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
+    new Record(this, "mx1", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
       priority: 1,
-			value: "in1-smtp.messagingengine.com.",
-			comment: "Managed by Terraform",
-		});
+      value: "in1-smtp.messagingengine.com.",
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "mx2", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
+    new Record(this, "mx2", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
       priority: 5,
-			value: "in2-smtp.messagingengine.com.",
-			comment: "Managed by Terraform",
-		});
+      value: "in2-smtp.messagingengine.com.",
+      comment: "Managed by Terraform",
+    });
 
-		this.addTextRecord(
-			"spf",
-			"@",
-      'v=spf1 include:spf.messagingengine.com ~all',
-		);
+    this.addTextRecord(
+      "spf",
+      "@",
+      "v=spf1 include:spf.messagingengine.com ~all",
+    );
 
-		for (let i = 1; i <= 3; i++) {
-			this.addCNameRecord(
-				`dkim${i}`,
-				`fm${i}._domainkey`,
+    for (let i = 1; i <= 3; i++) {
+      this.addCNameRecord(
+        `dkim${i}`,
+        `fm${i}._domainkey`,
         `fm${i}.${this.cloudflareZone.zone}.dkim.fmhosted.com`,
-			);
+      );
     }
 
     this.addCNameRecord(
@@ -196,10 +201,10 @@ export class ManagedDomain extends Construct {
       `mesmtp.${this.cloudflareZone.zone}.dkim.fmhosted.com`,
     );
 
-		new Record(this, "srv-submission", {
-			zoneId: this.cloudflareZone.id,
+    new Record(this, "srv-submission", {
+      zoneId: this.cloudflareZone.id,
       name: "srv-submission",
-			type: "SRV",
+      type: "SRV",
       ttl: 3600,
       data: {
         name: this.cloudflareZone.zone,
@@ -210,14 +215,14 @@ export class ManagedDomain extends Construct {
         port: 587,
         target: "smtp.fastmail.com",
       },
-			comment: "Managed by Terraform",
-		});
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "srv-imap", {
-			zoneId: this.cloudflareZone.id,
+    new Record(this, "srv-imap", {
+      zoneId: this.cloudflareZone.id,
       name: "srv-imap",
-			type: "SRV",
-			ttl: 3600,
+      type: "SRV",
+      ttl: 3600,
       data: {
         name: this.cloudflareZone.zone,
         service: "_imap",
@@ -227,14 +232,14 @@ export class ManagedDomain extends Construct {
         port: 0,
         target: ".",
       },
-			comment: "Managed by Terraform",
-		});
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "srv-imaps", {
-			zoneId: this.cloudflareZone.id,
+    new Record(this, "srv-imaps", {
+      zoneId: this.cloudflareZone.id,
       name: "srv-imaps",
-			type: "SRV",
-			ttl: 3600,
+      type: "SRV",
+      ttl: 3600,
       data: {
         name: this.cloudflareZone.zone,
         service: "_imaps",
@@ -244,11 +249,11 @@ export class ManagedDomain extends Construct {
         port: 993,
         target: "imap.fastmail.com",
       },
-			comment: "Managed by Terraform",
-		});
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "srv-jmap", {
-			zoneId: this.cloudflareZone.id,
+    new Record(this, "srv-jmap", {
+      zoneId: this.cloudflareZone.id,
       name: "srv-jmap",
       type: "SRV",
       ttl: 3600,
@@ -298,7 +303,6 @@ export class ManagedDomain extends Construct {
       comment: "Managed by Terraform",
     });
 
-
     new Record(this, "srv-caldav", {
       zoneId: this.cloudflareZone.id,
       name: "srv-caldav",
@@ -333,7 +337,7 @@ export class ManagedDomain extends Construct {
       comment: "Managed by Terraform",
     });
 
-		return this;
+    return this;
   }
 
   enableBumpEmail(config: BumpEmailConfig): ManagedDomain {
@@ -359,89 +363,93 @@ export class ManagedDomain extends Construct {
 
     this.addTextRecord("spf", "@", "v=spf1 include:spf.bump.email ~all");
 
-    this.addTextRecord("dkim", "bump._domainkey", `v=DKIM1; p=${config.domainKey}`);
+    this.addTextRecord(
+      "dkim",
+      "bump._domainkey",
+      `v=DKIM1; p=${config.domainKey}`,
+    );
 
     return this;
   }
 
-	enableGSuite(config: GSuiteConfig): ManagedDomain {
-		new Record(this, "mx1", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
-			priority: 1,
-			value: "aspmx.l.google.com.",
-			comment: "Managed by Terraform",
-		});
+  enableGSuite(config: GSuiteConfig): ManagedDomain {
+    new Record(this, "mx1", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
+      priority: 1,
+      value: "aspmx.l.google.com.",
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "mx2", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
-			priority: 5,
-			value: "alt1.aspmx.l.google.com.",
-			comment: "Managed by Terraform",
-		});
+    new Record(this, "mx2", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
+      priority: 5,
+      value: "alt1.aspmx.l.google.com.",
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "mx3", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
-			priority: 5,
-			value: "alt2.aspmx.l.google.com.",
-			comment: "Managed by Terraform",
-		});
+    new Record(this, "mx3", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
+      priority: 5,
+      value: "alt2.aspmx.l.google.com.",
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "mx4", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
-			priority: 10,
-			value: "alt3.aspmx.l.google.com.",
-			comment: "Managed by Terraform",
-		});
+    new Record(this, "mx4", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
+      priority: 10,
+      value: "alt3.aspmx.l.google.com.",
+      comment: "Managed by Terraform",
+    });
 
-		new Record(this, "mx5", {
-			zoneId: this.cloudflareZone.id,
-			name: "@",
-			type: "MX",
-			ttl: 3600,
-			priority: 10,
-			value: "alt4.aspmx.l.google.com.",
-			comment: "Managed by Terraform",
-		});
+    new Record(this, "mx5", {
+      zoneId: this.cloudflareZone.id,
+      name: "@",
+      type: "MX",
+      ttl: 3600,
+      priority: 10,
+      value: "alt4.aspmx.l.google.com.",
+      comment: "Managed by Terraform",
+    });
 
-		this.addTextRecord(
-			"dkim",
-			"google._domainkey",
+    this.addTextRecord(
+      "dkim",
+      "google._domainkey",
       `v=DKIM1; k=rsa; p=${config.domainKey}`,
-		);
+    );
 
-		this.addTextRecord(
-			"spf",
-			"@",
+    this.addTextRecord(
+      "spf",
+      "@",
       `v=spf1 include:_spf.google.com ${config.spfIncludes
-				.map((include) => `include:${include}`)
+        .map((include) => `include:${include}`)
         .join(" ")} ~all`,
-		);
+    );
 
-		return this;
-	}
+    return this;
+  }
 
-	setupRebrandly(subdomain: string): ManagedDomain {
-		this.addARecord("rebrandly", subdomain, "52.72.49.79");
+  setupRebrandly(subdomain: string): ManagedDomain {
+    this.addARecord("rebrandly", subdomain, "52.72.49.79");
 
-		return this;
-	}
+    return this;
+  }
 
-	setupShortIO(subdomain: string): ManagedDomain {
-		this.addARecord("shortio1", subdomain, "52.21.33.16");
-		this.addARecord("shortio2", subdomain, "52.59.165.42");
+  setupShortIO(subdomain: string): ManagedDomain {
+    this.addARecord("shortio1", subdomain, "52.21.33.16");
+    this.addARecord("shortio2", subdomain, "52.59.165.42");
 
-		return this;
-	}
+    return this;
+  }
 }
--- projects/core-infrastructure/dns/src/domains/rawko.de.ts
+++ projects/core-infrastructure/dns/src/domains/rawko.de.ts
@@ -2,9 +2,11 @@ import { Construct } from "constructs";
 import { ManagedDomain, Registrar } from "../dnsProvider";
 
 export default (scope: Construct): ManagedDomain => {
-	const managedDomain = new ManagedDomain(scope, "rawko.de", Registrar.Gandi);
+  const managedDomain = new ManagedDomain(scope, "rawko.de", Registrar.Gandi);
 
-	managedDomain.discourageEmail().setupShortIO("@");
+  managedDomain
+    .discourageEmail()
+    .addCNameRecord("vista-social", "@", "vist.ly", false);
 
-	return managedDomain;
+  return managedDomain;
 };
