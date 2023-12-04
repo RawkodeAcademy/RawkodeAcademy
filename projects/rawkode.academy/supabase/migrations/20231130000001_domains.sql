39b101b4b00cd3e1d6c06fa335900b0fe54b2b9f
David Flanagan <david@rawkode.dev>
2023-12-04 22:26:48 +0000

    fix: citext adoption

--- projects/rawkode.academy/supabase/migrations/20231130000001_domains.sql
+++ projects/rawkode.academy/supabase/migrations/20231130000001_domains.sql
@@ -1,32 +1,11 @@
 -- GitHub Handles
-create domain "github_handle"
-  as text
-  constraint "github_handle_length"
-    check (length(value) <= 39);
+create domain "github_handle" as citext constraint "github_handle_length" check (length(value) <= 39);
 
 -- x handles
-create domain "x_handle"
-  as text
-  constraint "x_handle_length"
-    check (
-      (length(value) >= 4)
-      and
-      (length(value) <= 15)
-);
+create domain "x_handle" as citext constraint "x_handle_length" check ((length(value) >= 4)
+	and (length(value) <= 15));
 
 -- youtube handles
-create domain "youtube_handle"
-  as text
-  constraint "youtube_handle_length"
-    check (
-      (length(value) >= 3)
-      and
-      (length(value) <= 30)
-);
+create domain "youtube_handle" as citext constraint "youtube_handle_length" check ((length(value) >= 3)
+	and (length(value) <= 30));
 
--- chapters
--- primarily used to mark up segments of podcasts and video content
-create type "chapter" as (
-  "time" interval,
-  "title" text
-);
--- projects/rawkode.academy/supabase/migrations/20231130000090_graphql.sql
+++ projects/rawkode.academy/supabase/migrations/20231130000090_graphql.sql
@@ -2,16 +2,15 @@ comment on schema public is e'@graphql({"inflect_names": true, "max_rows": 512})
 
 --- Let's flatten ShowHosts
 create function "hosts"("shows")
-    returns setof "people"
-    language sql
-    as
-$$
-    select
-        p
-    from
-        "show_hosts" sh
-        join "people" p
-            on sh."person_id" = "p".github_handle
-    where
-        sh."show_id" = $1.slug
+	returns setof "people"
+	language sql
+	as $$
+	select
+		p
+	from
+		"show_hosts" sh
+		join "people" p on sh."person_id" = "p".github_handle
+	where
+		sh."show_id" = $1.slug
 $$;
+
