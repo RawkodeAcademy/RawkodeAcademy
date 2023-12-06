af2c0a8d81ec986840cdb47a531bc7f848134062
David Flanagan <david@rawkode.dev>
2023-12-06 14:17:15 +0000

    feat: adopt nix flakes

--- projects/rawkode.academy/web/astro.config.mjs
+++ projects/rawkode.academy/web/astro.config.mjs
@@ -1,11 +1,11 @@
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind(), vue()],
-  adapter: cloudflare()
+	output: "hybrid",
+	integrations: [tailwind(), vue()],
+	adapter: cloudflare(),
 });
