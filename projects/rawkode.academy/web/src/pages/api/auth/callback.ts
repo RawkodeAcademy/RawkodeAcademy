722392971a9553e886e0ed64082cd122ae5a9c85
David Flanagan <david@rawkode.dev>
2023-12-07 14:35:01 +0000

    fix: auth trigger / sync and article author view

--- projects/rawkode.academy/web/src/pages/api/auth/callback.ts
+++ projects/rawkode.academy/web/src/pages/api/auth/callback.ts
@@ -4,13 +4,18 @@ import { supabase } from "../../../lib/supabase";
 export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 	const authCode = url.searchParams.get("code");
 
+	console.log(`Handling authCode ${authCode}`);
+
 	if (!authCode) {
 		return new Response("No code provided", { status: 400 });
 	}
 
+	console.log(`Auth Code: ${authCode}`);
+
 	const { data, error } = await supabase.auth.exchangeCodeForSession(authCode);
 
 	if (error) {
+		console.log(error);
 		return new Response(error.message, { status: 500 });
 	}
 
