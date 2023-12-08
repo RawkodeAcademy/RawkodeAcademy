c2353a7e8edd2a046232b4875d233347b5cc347a
David Flanagan <david@rawkode.dev>
2023-12-08 21:18:44 +0000

    chore: clean up old projects

--- projects/rawkode.academy/new/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import cloudflare from "@astrojs/cloudflare";
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-	output: "hybrid",
-	adapter: cloudflare({ mode: "directory", imageService: "cloudflare" }),
-});
