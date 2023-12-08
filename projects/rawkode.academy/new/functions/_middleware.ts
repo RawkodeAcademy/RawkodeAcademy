9b6a25d0ce0439bf8a8fdb2cf2d8ce3f4dc2584f
David Flanagan <david@rawkode.dev>
2023-12-08 21:18:44 +0000

    chore: clean up old projects

--- projects/rawkode.academy/new/functions/_middleware.ts
+++
@@ -1,5 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  const response = await context.next();
-  response.headers.set("X-Rawkode", "I AM HERE");
-  return response;
-};
--- projects/rawkode.academy/website/src/content/authors/rawkode.md
+++
@@ -1,4 +0,0 @@
----
-id: rawkode
-name: David Flanagan
----
