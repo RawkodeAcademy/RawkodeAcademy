f11bcd7fcbe337098e8b61ae8b9a18d727224193
David Flanagan <david@rawkode.dev>
2023-12-10 14:30:37 +0000

    feat: nix flake additions

--- projects/rawkode.academy/web/src/lib/supabase.ts
+++ projects/rawkode.academy/web/src/lib/supabase.ts
@@ -4,7 +4,7 @@ import type { Database } from "../database.types";
 
 export const supabase = createClient<Database>(
 	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_KEY,
+	import.meta.env.SUPABASE_ANON_KEY,
 	{
 		auth: {
 			flowType: "pkce",
