e564e889b2a2594265e02c5f36db86d3f82ae80e
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2023-12-12 13:30:29 +0100

    fix: change output back to "hybrid"

--- projects/rawkode.academy/web/astro.config.mjs
+++ projects/rawkode.academy/web/astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   integrations: [tailwind(), vue(), react()],
   adapter: cloudflare()
 });
