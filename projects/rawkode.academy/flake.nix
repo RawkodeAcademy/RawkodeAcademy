db9ce4740fe0d910cce089e0a71c2c220d563697
David Flanagan <david@rawkode.dev>
2023-12-17 22:32:35 +0000

    feat: add initial devenv

--- projects/rawkode.academy/flake.nix
+++ projects/rawkode.academy/flake.nix
@@ -1,21 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/release-23.11";
+    devenv.url = "github:cachix/devenv/v0.6.3";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
           pkgs = import nixpkgs {
             inherit system;
           };
-        in
-        with pkgs; {
-          devShells.default = mkShell
-            {
-              buildInputs = [
+        in with pkgs; {
+          devShells.default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+
+              modules = [{
+
+              packages = [
                 biome
                 bun
                 libiconv
@@ -26,6 +33,9 @@
               ] ++ lib.optionals stdenv.isDarwin [
                 darwin.apple_sdk.frameworks.SystemConfiguration
               ];
+
+              processes.run.exec = "supabase start";
+              }];
             };
         }
       );
