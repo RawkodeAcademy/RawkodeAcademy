295b076cc519fc617584b8d22417b9e7e51b755f
David Flanagan <david@rawkode.dev>
2023-12-18 21:21:05 +0000

    fix: don't use Supabase AllInOne

--- projects/dagger/supabase/main.go
+++ projects/dagger/supabase/main.go
@@ -19,6 +19,7 @@ const JWT_SECRET = "your-super-secret-jwt-token-with-at-least-32-characters-long
 const JWT_EXPIRY = "3600"
 const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE"
 const SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
+const ADMIN_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3VwYWJhc2VfYWRtaW4iLCJpc3MiOiJzdXBhYmFzZS1kZW1vIiwiaWF0IjoxNjQxNzY5MjAwLCJleHAiOjE3OTk1MzU2MDB9.Y9mSNVuTw2TdfryoaqM5wySvwQemGGWfSe9ixcklVfM"
 
 const LOGFLARE_LOGGER_BACKEND_API_KEY = "your-super-secret-and-long-logflare-key"
 const LOGFLARE_API_KEY = "your-super-secret-and-long-logflare-key"
@@ -113,13 +114,11 @@ func (m *Supabase) postgres() *Service {
 
 	return dag.
 		Container().
-		From("public.ecr.aws/supabase/postgres:aio-15.1.0.152").
+		From("public.ecr.aws/supabase/postgres:15.1.0.152").
 		WithEnvVariable("POSTGRES_PORT", "5432").
 		WithEnvVariable("POSTGRES_USERNAME", POSTGRES_USERNAME).
 		WithEnvVariable("POSTGRES_PASSWORD", POSTGRES_PASSWORD).
 		WithEnvVariable("POSTGRES_DB", "supabase").
-    WithEnvVariable("ANON_KEY", ANON_KEY).
-    WithEnvVariable("SERVICE_ROLE_KEY", SERVICE_ROLE_KEY).
 		WithEnvVariable("JWT_SECRET", JWT_SECRET).
 		WithEnvVariable("JWT_EXP", JWT_EXPIRY).
 		WithMountedFile("/docker-entrypoint-initdb.d/init-scripts/98-webhooks.sql", webhookSql).
