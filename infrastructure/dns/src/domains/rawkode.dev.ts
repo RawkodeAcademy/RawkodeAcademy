6829dccb038ab7dde1d1bbdfca95d947880472a6
David Flanagan <david@rawkode.dev>
2024-01-24 16:54:09 +0000

    fix: revert to gsuite

--- infrastructure/dns/src/domains/rawkode.dev.ts
+++ infrastructure/dns/src/domains/rawkode.dev.ts
@@ -7,8 +7,9 @@ export default (scope: Construct): ManagedDomain => {
 		"rawkode.dev",
 		Registrar.Cloudflare,
 	)
-		.enableMicrosoft365({
-			txtVerification: "MS=ms86636100",
+		.enableGSuite({
+			domainKey:
+				"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhiWae1/qb61lZzpUW8pnwS+97DkHVdjz42VV27sGvA1NRdXNh1qN2Yh7p8na6riahggJO+YLKuPSK35NUPqceDZCvtv0jdnO3drifcmL8ZeI8xcn421H19UrUajV8Qow9ZnErLMlHlbr7zBl6aJthUYtGEACTsWUwTY3mCeM3r5iEDJBPpTu8GGkGoBktJJx5GoadQ7T821J1c4LqXU9ZF8Gqtq0rL3QUf4aQxsHbJeJQLzrDmpDV6xHTJOBk+JNRYD2DZWv0LxBD3eJGa40gvqfsx5SeD6XlSX5JuzHJjiCUBdjL4CMtdEt0lQWetF22TQ9wCz8hwhwzv7qGW099wIDAQAB",
 			spfIncludes: [],
 		})
 		.addTextRecord(
