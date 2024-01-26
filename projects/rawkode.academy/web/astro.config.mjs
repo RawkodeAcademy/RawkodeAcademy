521ffe983b17532c172c0bd5143846b843c350f1
David Flanagan <david@rawkode.dev>
2024-01-26 16:21:09 +0000

    fix: dependency updates

--- projects/rawkode.academy/web/astro.config.mjs
+++ projects/rawkode.academy/web/astro.config.mjs
@@ -4,9 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
 	integrations: [tailwind(), vue(), react()],
-	adapter: cloudflare()
+	adapter: cloudflare({ mode: "advanced" })
 });
