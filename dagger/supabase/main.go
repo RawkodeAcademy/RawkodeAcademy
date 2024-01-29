c6bded728720518dc5f71b17786357e756a55fab
David Flanagan <david@rawkode.dev>
2024-01-29 17:43:51 +0000

    fix: squash bugs

--- dagger/supabase/main.go
+++ dagger/supabase/main.go
@@ -17,7 +17,7 @@ type Supabase struct {
 // All in one image it is ...
 // Want the 'producation' code? Checkout commit
 //   - dcc6262a11d455c2b987a80434291f37b66e14c6
-func (m *Supabase) DevStack() (*Supabase, err) {
+func (m *Supabase) DevStack() (*Supabase, error) {
 	postgresPassword, err := password.Generate(64, 10, 10, false, false)
 	if err != nil {
 		return nil, err
@@ -37,19 +37,19 @@ func (m *Supabase) DevStack() (*Supabase, err) {
 		"exp":  jwt.NewNumericDate(now.Add(time.Duration(4) * time.Hour)),
 	})
 
-	m.anonKey, err := token.SignedString(jwtSecret)
+	m.AnonKey, err = token.SignedString(jwtSecret)
 	if err != nil {
 		return nil, err
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"role": "anon",
 		"iss":  "supabase",
 		"iat":  jwt.NewNumericDate(now),
 		"exp":  jwt.NewNumericDate(now.Add(time.Duration(4) * time.Hour)),
 	})
 
-	m.ServiceKey, err := token.SignedString(jwtSecret)
+	m.ServiceKey, err = token.SignedString(jwtSecret)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +58,7 @@ func (m *Supabase) DevStack() (*Supabase, err) {
 		From("public.ecr.aws/supabase/postgres:aio-15.1.0.153").
 		WithEnvVariable("POSTGRES_PASSWORD", postgresPassword).
 		WithEnvVariable("JWT_SECRET", jwtSecret).
-		WithEnvVariable("ANON_KEY", m.anonKey).
+		WithEnvVariable("ANON_KEY", m.AnonKey).
 		WithEnvVariable("SERVICE_ROLE_KEY", m.ServiceKey).
 		WithEnvVariable("MACHINE_TYPE", "shared_cpu_1x_512m").
 		WithEnvVariable("DATA_VOLUME_MOUNTPOINT", "/data").
@@ -66,5 +66,5 @@ func (m *Supabase) DevStack() (*Supabase, err) {
 		WithExposedPort(8000).
 		AsService()
 
-	return m
+	return m, nil
 }
