7a20624e486d6d2cca09175c176a63ab7e098999
David Flanagan <david@rawkode.dev>
2024-01-31 22:24:27 +0000

    wip: clerk pages

--- projects/rawkode-academy/web/src/env.d.ts
+++ projects/rawkode-academy/web/src/env.d.ts
@@ -1,30 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-type KindeUser = {
-  family_name: string;
-  given_name: string;
-  picture: string;
-  email: string;
-  id: string;
-};
-
-declare namespace App {
-  interface Locals extends NetlifyLocals {
-    user: KindeUser;
-    acStateKey: unknown;
-    idToken: unknown;
-    accessToken: unknown;
-    refreshToken: unknown;
-  }
-}
-
 interface ImportMetaEnv {
-  readonly KINDE_CLIENT_ID: string;
-  readonly KINDE_CLIENT_SECRET: string;
-  readonly KINDE_ISSUER_URL: string;
-  readonly KINDE_POST_LOGOUT_REDIRECT_URL: string;
-  readonly KINDE_REDIRECT_URL: string;
+	readonly CLERK_PUBLIC_KEY: string;
+	readonly CLERK_SECRET_KEY: string;
 }
 
 interface ImportMeta {
--- projects/rawkode-academy/web/src/middleware.ts
+++ projects/rawkode-academy/web/src/middleware.ts
@@ -1,14 +1,3 @@
-import { defineMiddleware } from "astro:middleware";
-import { sessionManager } from "./lib/kinde";
+import { clerkMiddleware } from "astro-clerk-auth/server";
 
-export const onRequest = defineMiddleware(async ({ cookies, locals }, next) => {
-  const session = sessionManager(cookies);
-
-  locals.user = (await session.getSessionItem("user")) as KindeUser;
-  locals.acStateKey = await session.getSessionItem("ac-state-key");
-  locals.idToken = await session.getSessionItem("id_token");
-  locals.accessToken = await session.getSessionItem("access_token");
-  locals.refreshToken = await session.getSessionItem("refresh_token");
-
-  return next();
-});
+export const onRequest = clerkMiddleware();
