65faba7fe57ce4771bf693ab575c79aa7c1eebbe
David Flanagan <david@rawkode.dev>
2024-03-01 12:24:36 +0000

    chore: remove pkgx

--- projects/rawkode-academy/shows-service/grafbase/resolvers/shows.ts
+++ projects/rawkode-academy/shows-service/grafbase/resolvers/shows.ts
@@ -1,15 +1,12 @@
 import { createClient } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "../../drizzle/schema";
-import { getSecrets } from "../../utils/secrets";
 import { type Resolver } from "../generated/index";
-
-const secrets = await getSecrets();
 
 const resolver: Resolver["Query.shows"] = async () => {
 	const client = createClient({
-		url: secrets.tursoUrl,
-		authToken: secrets.tursoToken,
+		url: process.env.TURSO_URL as string,
+		authToken: process.env.TURSO_TOKEN as string,
 	});
 
 	const db = drizzle(client, { schema });
