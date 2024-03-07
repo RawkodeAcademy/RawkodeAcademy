2f93c69301397c12467d4c8e749b65508a209a53
David Flanagan <david@rawkode.dev>
2024-03-07 11:01:06 +0000

    feat(infrastructure): setting up Stripe custom domain

--- infrastructure/dns/src/domains/rawkode.academy.ts
+++ infrastructure/dns/src/domains/rawkode.academy.ts
@@ -11,45 +11,51 @@ export default (scope: Construct): ManagedDomain => {
 	managedDomain
 		.enableGSuite({
 			domainKey:
-				"p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0htq+un78kY5mLFMLYmkb0Dl9uzX5P0RwhwY6ADVCpL+SI7zZYOyY4uPpRq44nm+H1iqet8wfjLlvnn1VrxB5ivwJisNsaleMZnkNPPXOZ+Y9lDFxqQLHUBzEB3dmHm7/HD2wcEHiConR2CFZ7888dDfMoBzpCifGdqDPsGJR2vCVNYHPDvd9fs6wwZQszJASWWxeMWV1U7rN9GqPHBMvgGJ4HfyWqOPrGEzWAdJ8+y98fhzDchejEnFOP7AtccqifoM7jpOwRDitjW+RL6gofYOJrJG48lSFSSEw25KWQ6yqEaT/Q1cXzIyOsvXuMNQ9UcPUpg8jDboor9vj852dwIDAQAB",
+				"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0htq+un78kY5mLFMLYmkb0Dl9uzX5P0RwhwY6ADVCpL+SI7zZYOyY4uPpRq44nm+H1iqet8wfjLlvnn1VrxB5ivwJisNsaleMZnkNPPXOZ+Y9lDFxqQLHUBzEB3dmHm7/HD2wcEHiConR2CFZ7888dDfMoBzpCifGdqDPsGJR2vCVNYHPDvd9fs6wwZQszJASWWxeMWV1U7rN9GqPHBMvgGJ4HfyWqOPrGEzWAdJ8+y98fhzDchejEnFOP7AtccqifoM7jpOwRDitjW+RL6gofYOJrJG48lSFSSEw25KWQ6yqEaT/Q1cXzIyOsvXuMNQ9UcPUpg8jDboor9vj852dwIDAQAB",
 			spfIncludes: [],
 		})
 		.addTextRecord(
 			"google-site-verification",
 			"@",
-			"google-site-verification=dlh9jxVzubowYFoVO82naJOotuUwY8zNG2VYGWlDhsU",
+			"google-site-verification=dlh9jxVzubowYFoVO82naJOotuUwY8zNG2VYGWlDhsU"
+		)
+		.addCNameRecord("stripe", "billing", "hosted-checkout.stripecdn.com")
+		.addTextRecord(
+			"stripe-acme",
+			"_acme-challenge.billing",
+			"sJV8MAy9curK26zz05xdgnDL3X6uf-2IEdua78oHimw"
 		)
 		.addARecord("web", "@", "185.230.63.107")
 		.addCNameRecord("www", "www", "pointing.wixdns.net")
 		.addTextRecord(
 			"_atproto",
 			"_atproto",
-			"did=did:plc:qtpysarntxepux4to4dr4hgr",
+			"did=did:plc:qtpysarntxepux4to4dr4hgr"
 		)
 		.addCNameRecord(
 			"clerk-accounts-portal",
 			"accounts",
-			"accounts.clerk.services",
+			"accounts.clerk.services"
 		)
 		.addCNameRecord(
 			"clerk-frontend-api",
 			"clerk",
-			"frontend-api.clerk.services",
+			"frontend-api.clerk.services"
 		)
 		.addCNameRecord(
 			"clerk-dkim-1",
 			"clk._domainkey",
-			"dkim1.yo4jsvea9l19.clerk.services",
+			"dkim1.yo4jsvea9l19.clerk.services"
 		)
 		.addCNameRecord(
 			"clerk-dkim-2",
 			"clk2._domainkey",
-			"dkim2.yo4jsvea9l19.clerk.services",
+			"dkim2.yo4jsvea9l19.clerk.services"
 		)
 		.addCNameRecord(
 			"clerk-mail",
 			"clkmail",
-			"mail.yo4jsvea9l19.clerk.services",
+			"mail.yo4jsvea9l19.clerk.services"
 		);
 
 	return managedDomain;
