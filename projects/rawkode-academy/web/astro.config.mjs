947ba28cd08bee57212c5dc94436e8bff8f82feb
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-07-07 01:22:22 +0200

    feat: zoomable images
    closes #115

--- projects/rawkode-academy/web/astro.config.mjs
+++ projects/rawkode-academy/web/astro.config.mjs
@@ -3,9 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
-	integrations: [tailwind(), vue(), react()],
-	adapter: cloudflare({ mode: "advanced" }),
+	integrations: [tailwind(), vue(), react(), mdx()],
+	adapter: cloudflare({
+		mode: "advanced",
+	}),
 });
