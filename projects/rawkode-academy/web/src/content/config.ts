cb5cf81fb16175f863a41666b5f43c1c6d48ac23
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-07-07 23:03:57 +0200

    feat(web): optional subtitles for blog posts

--- projects/rawkode-academy/web/src/content/config.ts
+++ projects/rawkode-academy/web/src/content/config.ts
@@ -1,9 +1,10 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-	type: 'content',
+	type: "content",
 	schema: z.object({
 		title: z.string(),
+		subtitle: z.string().optional(),
 		publishedAt: z.coerce.date(),
 		updatedAt: z.coerce.date().optional(),
 		isDraft: z.boolean().default(true),
