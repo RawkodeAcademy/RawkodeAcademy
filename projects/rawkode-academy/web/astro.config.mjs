1a1cc4dfb89405ee6b4d3ee262324634ad637170
Todd Martin <kj4ntv@gmail.com>
2024-07-07 13:45:41 -0700

    use rehype-external-links to handle opening a link in a new tab

--- projects/rawkode-academy/web/astro.config.mjs
+++ projects/rawkode-academy/web/astro.config.mjs
@@ -3,9 +3,20 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
 	output: "hybrid",
 	integrations: [tailwind(), vue(), react()],
 	adapter: cloudflare({ mode: "advanced" }),
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					"target": '_blank'
+				}
+			]
+		]
+	}
 });
