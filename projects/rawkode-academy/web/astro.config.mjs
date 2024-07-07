713103bf37aebab22b4c0ad6d9737a25d01d4e53
Todd Martin <kj4ntv@gmail.com>
2024-07-07 13:45:41 -0700

    use rehype-external-links to handle opening a link in a new tab

--- projects/rawkode-academy/web/astro.config.mjs
+++ projects/rawkode-academy/web/astro.config.mjs
@@ -1,9 +1,9 @@
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
