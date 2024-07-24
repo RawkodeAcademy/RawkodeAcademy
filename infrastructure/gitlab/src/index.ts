d2d69f285e62ab498f3b855eb719ed1ca68de488
David Flanagan <david@rawkode.dev>
2024-07-24 18:52:15 +0100

    feat(gitlab): automate group labels

--- infrastructure/gitlab/src/index.ts
+++ infrastructure/gitlab/src/index.ts
@@ -2,6 +2,7 @@ import { App, HttpBackend, TerraformStack } from "cdktf";
 import { Construct } from "constructs";
 import { GitlabProvider } from "@generatedProviders/gitlab/provider";
 import { Project } from "@generatedProviders/gitlab/project";
+import { GroupLabel } from "@generatedProviders/gitlab/group-label";
 import { DataGitlabGroup } from "@generatedProviders/gitlab/data-gitlab-group";
 
 class GitLab extends TerraformStack {
@@ -21,6 +22,57 @@ class GitLab extends TerraformStack {
 			namespaceId: group.groupId,
 			description: "Rawkode Academy Monorepository",
 			visibilityLevel: "public",
+		});
+
+		new GroupLabel(this, "label-status-backlog", {
+			name: "status::Backlog",
+			color: "#ed9121",
+			description: "These issues are ready to be picked up and worked on.",
+			group: group.id,
+		});
+
+		new GroupLabel(this, "label-status-triage", {
+			name: "status::Needs Triage",
+			color: "#ed9121",
+			description:
+				"These issues need to be triaged by the team and are not ready to work on.",
+			group: group.id,
+		});
+
+		new GroupLabel(this, "label-status-picked-up", {
+			name: "status::Picked Up",
+			color: "#ed9121",
+			description:
+				"These issues have been assigned and will be worked on shortly.",
+			group: group.id,
+		});
+
+		new GroupLabel(this, "label-status-in-review", {
+			name: "status::In Review",
+			color: "#ed9121",
+			description: "These issues are currently being reviewed by the team.",
+			group: group.id,
+		});
+
+		new GroupLabel(this, "label-type-operations", {
+			name: "type::Operations",
+			group: group.id,
+			color: "#6699cc",
+			description: "These issues are operational tasks.",
+		});
+
+		new GroupLabel(this, "label-type-defect", {
+			name: "type::Defect",
+			group: group.id,
+			color: "#6699cc",
+			description: "These issues are defects in the system.",
+		});
+
+		new GroupLabel(this, "label-type-enhancement", {
+			name: "type::Enhancement",
+			group: group.id,
+			color: "#6699cc",
+			description: "These issues are enhancements to the system.",
 		});
 	}
 }
