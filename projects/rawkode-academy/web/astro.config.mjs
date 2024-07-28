a78c9f3511f835bcb53fbb616ce23e934795b05b
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-07-28 03:51:53 +0200

    feat(web): sitemap
    - add sitemap
    - change robots.txt to dynamic robots.txt which links to the sitemap

--- projects/rawkode-academy/web/astro.config.mjs
+++ projects/rawkode-academy/web/astro.config.mjs
@@ -5,50 +5,47 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig, envField } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-	integrations: [tailwind(), vue(), react(), mdx()],
-	adapter: cloudflare({
-		mode: "advanced",
-	}),
-	site: import.meta.env.DEV ? "http://localhost:4321" : "https://rawkode.academy",
-	experimental: {
-		serverIslands: true,
-		env: {
-			validateSecrets: true,
-			schema: {
-				REDIRECT_URL: envField.string({
-					context: "server",
-					access: "public",
-					optional: false,
-				}),
-				WORKOS_CLIENT_ID: envField.string({
-					context: "server",
-					access: "public",
-					optional: false,
-				}),
-				WORKOS_API_KEY: envField.string({
-					context: "server",
-					access: "secret",
-					optional: false,
-				}),
-				WORKOS_COOKIE_PASSWORD: envField.string({
-					context: "server",
-					access: "secret",
-					optional: false,
-				}),
-			},
-		},
-	},
-	markdown: {
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: "_blank",
-				},
-			],
-		],
-	},
+  output: "hybrid",
+  integrations: [tailwind(), vue(), react(), mdx(), sitemap()],
+  adapter: cloudflare({
+    mode: "advanced"
+  }),
+  site: import.meta.env.DEV ? "http://localhost:4321" : "https://rawkode.academy",
+  experimental: {
+    serverIslands: true,
+    env: {
+      validateSecrets: true,
+      schema: {
+        REDIRECT_URL: envField.string({
+          context: "server",
+          access: "public",
+          optional: false
+        }),
+        WORKOS_CLIENT_ID: envField.string({
+          context: "server",
+          access: "public",
+          optional: false
+        }),
+        WORKOS_API_KEY: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false
+        }),
+        WORKOS_COOKIE_PASSWORD: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false
+        })
+      }
+    }
+  },
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank"
+    }]]
+  }
 });
