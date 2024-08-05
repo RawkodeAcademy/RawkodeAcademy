88803d5c1dd72999dba382603dcd9c83cc902f50
David Flanagan <david@rawkode.dev>
2024-08-05 17:17:43 +0100

    feat(web): initial partnership page

---
+++ projects/rawkode-academy/web/src/components/stats/purple.vue
@@ -0,0 +1,30 @@
+<template>
+	<section class="relative mt-24 py-24 overflow-hidden bg-indigo-600 text-white">
+		<div class="container px-4 mx-auto">
+			<div class="flex flex-wrap -m-8">
+				<div class="w-full md:w-1/2 lg:w-1/4 p-8">
+					<h2 class="text-5xl font-bold font-heading tracking-px-n leading-tight">We've got some good numbers, and we're
+						only getting started.</h2>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/4 p-8">
+					<div class="md:w-56">
+						<h2 class="mb-3 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight">90+</h2>
+						<p class="text-lg font-medium leading-normal">Cloud Native projects covered</p>
+					</div>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/4 p-8">
+					<div class="md:w-56">
+						<h2 class="mb-3 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight">500+</h2>
+						<p class="text-lg font-medium leading-normal">Hours of content</p>
+					</div>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/4 p-8">
+					<div class="md:w-56">
+						<h2 class="mb-3 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight">1 Million+</h2>
+						<p class="text-lg font-medium leading-normal">views</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
