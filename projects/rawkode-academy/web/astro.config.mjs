cba34b80c90c75a6520a41fa45749d8097e999df
David Flanagan <david@rawkode.dev>
2024-08-06 09:36:36 +0100

    chore(web): remove biome from bun deps
    The Biome extension for VSCode searches for the dependency version
    before PATH, which causes it not to work on NixOS

--- projects/rawkode-academy/web/astro.config.mjs
+++ projects/rawkode-academy/web/astro.config.mjs
@@ -1,11 +1,11 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig, envField } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
