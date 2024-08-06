82fda7b7a012bb40b7322162f4bd2cc44b3af104
David Flanagan <david@rawkode.dev>
2024-08-06 22:24:24 +0100

    feat(web): add some sales pitch

--- projects/rawkode-academy/web/src/components/stats/simple.vue
+++ projects/rawkode-academy/web/src/components/stats/simple.vue
@@ -14,11 +14,11 @@ defineProps<Props>();
 
 <template>
 	<section class="bg-white dark:bg-gray-900">
-		<div class="px-4 py-8 mx-auto max-w-screen-xl sm:py-16 lg:px-6 xl:px-0">
-			<div class="mx-auto mb-8 max-w-screen-md text-center md:mb-16">
+		<div class="px-4 mx-auto max-w-screen-xl lg:px-6 xl:px-0">
+			<div class="mx-auto max-w-screen-md text-center">
 				<h2 class="text-3xl tracking-tight font-extrabold text-gray-900 md:text-4xl dark:text-white">{{ title }}</h2>
 			</div>
-			<div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+			<div class="max-w-screen-xl px-4 mx-auto text-center lg:py-16 lg:px-6">
 				<dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
 					<div v-for="stat in stats" class="flex flex-col items-center justify-center">
 						<dt class="mb-2 text-4xl md:text-5xl font-extrabold">{{ stat.value }}</dt>
