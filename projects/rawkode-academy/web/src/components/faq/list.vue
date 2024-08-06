5bebc9a78c5ad4a992edf92ba26f41ade27c2014
David Flanagan <david@rawkode.dev>
2024-08-06 22:34:11 +0100

    feat(web): partnership faq

---
+++ projects/rawkode-academy/web/src/components/faq/list.vue
@@ -0,0 +1,101 @@
+<template>
+	<section class="bg-white dark:bg-gray-900">
+		<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+			<h2
+				class="mb-6 lg:mb-8 text-3xl lg:text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+				Frequently asked questions</h2>
+			<div class="mx-auto max-w-screen-md">
+				<div id="accordion-flush" data-accordion="collapse"
+					data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
+					data-inactive-classes="text-gray-500 dark:text-gray-400">
+					<h2 id="accordion-flush-heading-1">
+						<button type="button"
+							class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-900 bg-white border-b border-gray-200 dark:border-gray-700 dark:bg-gray-900 dark:text-white"
+							data-accordion-target="#accordion-flush-body-1" aria-expanded="true"
+							aria-controls="accordion-flush-body-1">
+							<span>Do you offer monthly partnerships?</span>
+							<svg data-accordion-icon="" class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"></path>
+							</svg>
+						</button>
+					</h2>
+					<div id="accordion-flush-body-1" class="" aria-labelledby="accordion-flush-heading-1">
+						<div class="py-5 border-b border-gray-200 dark:border-gray-700">
+							<p class="mb-2 text-gray-500 dark:text-gray-400">No. It's very difficult for us to work together in such a
+								short amount of time. In-order to see real results from our collaboration, it requires a long-term
+								commitment from us both. I want to help you, I can't do that in 30 days.</p>
+						</div>
+					</div>
+					<h2 id="accordion-flush-heading-2">
+						<button type="button"
+							class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+							data-accordion-target="#accordion-flush-body-2" aria-expanded="false"
+							aria-controls="accordion-flush-body-2">
+							<span>Do we need to pay a year upfront?</span>
+							<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"></path>
+							</svg>
+						</button>
+					</h2>
+					<div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+						<div class="py-5 border-b border-gray-200 dark:border-gray-700">
+							<p class="mb-2 text-gray-500 dark:text-gray-400">On the lower-tiers, yes. If you wish to join us on the
+								Blend or Malt level, we're happy to discuss.</p>
+						</div>
+					</div>
+					<h2 id="accordion-flush-heading-3">
+						<button type="button"
+							class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+							data-accordion-target="#accordion-flush-body-3" aria-expanded="false"
+							aria-controls="accordion-flush-body-3">
+							<span>Will we get 10,000 leads?</span>
+							<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"></path>
+							</svg>
+						</button>
+					</h2>
+					<div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+						<div class="py-5 border-b border-gray-200 dark:border-gray-700">
+							<p class="mb-2 text-gray-500 dark:text-gray-400">I don't know! Will 10,000 people view our content?
+								Definitely. Is that better than 2000 people swinging by your booth for free swag? Absolutely.</p>
+							<p class="mb-2 text-gray-500 dark:text-gray-400">As we add more functionality to the platform, you'll get
+								anonymised reports about the content and the viewership. If the viewers opt-in to communication from
+								partners, you'll have the ability to connect with them too.</p>
+						</div>
+					</div>
+					<h2 id="accordion-flush-heading-4">
+						<button type="button"
+							class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+							data-accordion-target="#accordion-flush-body-4" aria-expanded="false"
+							aria-controls="accordion-flush-body-4">
+							<span>I have more questions!</span>
+							<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"></path>
+							</svg>
+						</button>
+					</h2>
+					<div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
+						<div class="py-5 border-b border-gray-200 dark:border-gray-700">
+							<p class="mb-2 text-gray-500 dark:text-gray-400">That's OK! <a class="text-primary"
+									href="/organizations/lets-chat">Book some
+									time</a> for us to chat.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
