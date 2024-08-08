acf0afdf9bc72d3f03c703889708d7a0ec21e0ae
David Flanagan <david@rawkode.dev>
2024-08-08 09:58:32 +0100

    feat(rpc): hooking up workos webhook for user registered

--- projects/rawkode-academy/rpc/devenv.nix
+++ projects/rawkode-academy/rpc/devenv.nix
@@ -8,9 +8,24 @@
       enable = true;
       install.enable = true;
     };
+		# Trigger.dev CLI needs npm
+		npm.enable = true;
   };
 
   languages.typescript.enable = true;
 
-  packages = with pkgs; [ biome hurl just ];
+  packages = with pkgs; [
+    biome
+    hurl
+    just
+  ];
+
+  # See: https://github.com/cloudflare/workerd/issues/1482
+  enterShell = ''
+    bun install
+    __patchTarget="./node_modules/@cloudflare/workerd-linux-64/bin/workerd"
+    if [[ -f "$__patchTarget" ]]; then
+    	${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 "$__patchTarget"
+    fi
+  '';
 }
