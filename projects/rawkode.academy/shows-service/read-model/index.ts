1afcd06c44f93a15a29d662acd065469e18651ae
David Flanagan <david@rawkode.dev>
2024-09-26 18:46:45 +0100

    feat(shows): print schema and deploy to Cloudflare

--- projects/rawkode.academy/shows-service/read-model/index.ts
+++ projects/rawkode.academy/shows-service/read-model/index.ts
@@ -1,26 +1,53 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { buildSchema } from 'drizzle-graphql';
-import { drizzle } from 'drizzle-orm/libsql';
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-import { client } from "../data-model/client";
-import * as dbSchema from '../data-model/schema';
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { startServerAndCreateCloudflareWorkersHandler } from "@as-integrations/cloudflare-workers";
+import { buildSchema } from "drizzle-graphql";
+import { drizzle } from "drizzle-orm/libsql";
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import * as dataSchema from "../data-model/schema";
+import { createClient } from "@libsql/client";
+import type { ExecutionContext, ExportedHandler } from "@cloudflare/workers-types";
 
-const db = drizzle(client, { schema: dbSchema });
+interface Env {
+	LIBSQL_URL: string;
+	LIBSQL_TOKEN: string;
+}
 
-const { entities } = buildSchema(db);
+interface Context {
+	token: string;
+}
 
-const schema = new GraphQLSchema({
-	query: new GraphQLObjectType({
-		name: 'Query',
-		fields: {
-			show: entities.queries.showsTableSingle,
-			shows: entities.queries.showsTable,
-		},
-  })
-});
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const client = createClient({
+			url: env.LIBSQL_URL as string,
+			authToken: env.LIBSQL_TOKEN as string,
+		});
 
-const server = new ApolloServer({ schema });
-const { url } = await startStandaloneServer(server);
+		const db = drizzle(client, { schema: dataSchema });
 
-console.log(`🚀 Server ready at ${url}`);
+		const { entities } = buildSchema(db);
+
+		const schema = new GraphQLSchema({
+			query: new GraphQLObjectType({
+				name: "Query",
+				fields: {
+					show: entities.queries.showsTableSingle,
+					shows: entities.queries.showsTable,
+				},
+			}),
+		});
+
+		const server = new ApolloServer<Context>({
+			schema,
+			introspection: true,
+			plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
+		});
+
+		return startServerAndCreateCloudflareWorkersHandler<Env, Context>(server, {
+			context: async ({ env, request, ctx }) => {
+				return { token: "secret" };
+			},
+		})(request, env, ctx);
+	},
+} satisfies ExportedHandler<Env>;
