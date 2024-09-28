19c6bdf9a468a20e80d46464683bc6c008ef16dd
David Flanagan <david@rawkode.dev>
2024-09-28 12:49:40 +0100

    chore(people): switch to graphql-yoga

--- projects/rawkode.academy/people-service/read-model/index.ts
+++ projects/rawkode.academy/people-service/read-model/index.ts
@@ -1,22 +1,21 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { createClient } from "@libsql/client";
 import { buildSchema } from "drizzle-graphql";
 import { drizzle } from "drizzle-orm/libsql";
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import { createYoga } from "graphql-yoga";
 import * as dataSchema from "../data-model/schema.ts";
 
 if (!Deno.env.has("LIBSQL_URL")) {
-  Deno.env.set("LIBSQL_URL", "http://localhost:2000");
+	Deno.env.set("LIBSQL_URL", "http://localhost:2000");
 }
 
 if (!Deno.env.has("LIBSQL_TOKEN")) {
-  Deno.env.set("LIBSQL_TOKEN", "");
+	Deno.env.set("LIBSQL_TOKEN", "");
 }
 
 const client = createClient({
-  url: Deno.env.get("LIBSQL_URL") || "",
-  authToken: Deno.env.get("LIBSQL_TOKEN"),
+	url: Deno.env.get("LIBSQL_URL") || "",
+	authToken: Deno.env.get("LIBSQL_TOKEN"),
 });
 
 const db = drizzle(client, { schema: dataSchema });
@@ -24,22 +23,18 @@ const db = drizzle(client, { schema: dataSchema });
 const { entities } = buildSchema(db);
 
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: {
-      person: entities.queries.peopleTableSingle,
-      people: entities.queries.peopleTable,
-    },
-  }),
+	query: new GraphQLObjectType({
+		name: "Query",
+		fields: {
+			person: entities.queries.peopleTableSingle,
+			people: entities.queries.peopleTable,
+		},
+	}),
 });
 
-const server = new ApolloServer({
-  schema,
-  introspection: true,
-});
+const yoga = createYoga({ schema });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+Deno.serve({
+	hostname: "0.0.0.0",
+	port: 3000,
+}, yoga);
