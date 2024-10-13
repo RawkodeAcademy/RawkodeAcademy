671e7a43dfe4ce1f3cad03702e296994e04d9a50
Joshua <joshua@Joshuas-MacBook-Pro-2.local>
2024-10-13 17:23:51 +0530

    update the dagger file with the secrets

--- daggerverse/gcp-secrets/main.go
+++ daggerverse/gcp-secrets/main.go
@@ -18,12 +18,12 @@ type GcpSecrets struct{}
 func (m *GcpSecrets) GetSecret(projectID string, secretName string,
 	//+optional
 	// +default="latest"
-	version string, credential *dagger.File) (string, error) {
+	version string, credential *dagger.Secret) (string, error) {
 
 	// Create the client.
 	ctx := context.Background()
 
-	json, err := credential.Contents(ctx)
+	json, err := credential.Plaintext(ctx)
 	if err != nil {
 		return "", err
 	}
@@ -59,14 +59,14 @@ func (m *GcpSecrets) GetSecrets(projectID string, secretNames []string,
 	//+optional
 	// +default="latest"
 	version string,
-	credential *dagger.File) ([]string, error) {
+	credential *dagger.Secret) ([]string, error) {
 
 	// Create the client.
 	ctx := context.Background()
 
 	var output []string
 
-	json, err := credential.Contents(ctx)
+	json, err := credential.Plaintext(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -99,45 +99,5 @@ func (m *GcpSecrets) GetSecrets(projectID string, secretNames []string,
 	}
 
 	return output, nil
-
-}
-
-//Passing the credentials via the secret
-
-func (m *GcpSecrets) GetSecretViaSecret(projectID string, secretName string,
-	//+optional
-	// +default="latest"
-	version string, credential *dagger.Secret) (string, error) {
-
-	// Create the client.
-	ctx := context.Background()
-
-	json, err := credential.Plaintext(ctx)
-	if err != nil {
-		return "", err
-	}
-	b := []byte(json)
-	client, err := secretmanager.NewClient(ctx, option.WithCredentialsJSON(b))
-	if err != nil {
-		return "", err
-	}
-	defer client.Close()
-
-	// Create the request to create the secret.
-	secretName = "projects/" + projectID + "/secrets/" + secretName + "/versions/" + version
-
-	// Build the request.
-	accessRequest := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: secretName,
-	}
-
-	// Call the API.
-	result, err := client.AccessSecretVersion(ctx, accessRequest)
-	if err != nil {
-		return "", err
-	}
-
-	// Print the secret payload.
-	return string(result.Payload.Data), nil
 
 }
