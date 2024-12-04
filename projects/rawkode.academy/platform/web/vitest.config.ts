8e9901e71ff6992d4503c5be3beda72189f18e92
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-12-04 01:27:27 +0100

    ops: upgrade astro to v5
    - notes:
      - vitest has to stay on 2.1.6 -> https://github.com/vitest-dev/vitest/releases/tag/v2.1.7
      - changed community link to Zulip

--- projects/rawkode.academy/platform/web/vitest.config.ts
+++ projects/rawkode.academy/platform/web/vitest.config.ts
@@ -1,18 +1,9 @@
 /// <reference types="vitest" />
-import {
-	getViteConfig,
-	type AstroInlineConfig,
-	type ViteUserConfig,
-} from "astro/config";
-
-// https://stackoverflow.com/a/74453378
-interface VitestConfigExport extends ViteUserConfig {
-	test: AstroInlineConfig;
-}
+import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
 	test: {
 		include: ["src/**/*.{spec,test}.{ts,tsx}"],
 		mockReset: true,
 	},
-} as VitestConfigExport);
+});
