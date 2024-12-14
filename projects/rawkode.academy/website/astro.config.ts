92b7d31e723c4e195d496c7de2f624d72d4fd40d
David Flanagan <david@rawkode.dev>
2024-12-14 09:20:32 +0000

    fix: minor tweaks

--- projects/rawkode.academy/website/astro.config.ts
+++ projects/rawkode.academy/website/astro.config.ts
@@ -10,51 +10,51 @@ import rehypeExternalLinks from "rehype-external-links";
 import rehypeMermaid from "rehype-mermaid";
 
 export default defineConfig({
-	output: "server",
-	integrations: [
-		expressiveCode({
-			themes: ["catppuccin-mocha", "catppuccin-latte"],
-		}),
-		mdx(),
-		react(),
-		sitemap(),
-		tailwind(),
-		vue(),
-	],
-	adapter: cloudflare({
-		imageService: "cloudflare",
-	}),
-	site: import.meta.env.DENO
-		? "http://localhost:4321"
-		: "https://rawkode.academy",
-	env: {
-		validateSecrets: true,
-		schema: {
-			ZITADEL_URL: envField.string({
-				context: "server",
-				access: "public",
-			}),
-			ZITADEL_CLIENT_ID: envField.string({
-				context: "server",
-				access: "public",
-			}),
-		},
-	},
-	security: {
-		checkOrigin: true,
-	},
-	markdown: {
-		rehypePlugins: [
-			[
-				rehypeMermaid,
-				{ strategy: "img-svg" },
-			],
-			[
-				rehypeExternalLinks,
-				{
-					target: "_blank",
-				},
-			],
-		],
-	},
+  output: "server",
+  integrations: [
+    expressiveCode({
+      themes: ["catppuccin-mocha", "catppuccin-latte"],
+    }),
+    mdx(),
+    react(),
+    sitemap(),
+    tailwind(),
+    vue(),
+  ],
+  adapter: cloudflare({
+    imageService: "cloudflare",
+  }),
+  site: import.meta.env.CF_PAGES_URL
+    ? import.meta.env.CF_PAGES_URL
+    : "http://localhost:4321",
+  env: {
+    validateSecrets: true,
+    schema: {
+      ZITADEL_URL: envField.string({
+        context: "server",
+        access: "public",
+      }),
+      ZITADEL_CLIENT_ID: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
+  security: {
+    checkOrigin: true,
+  },
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeMermaid,
+        { strategy: "img-svg" },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
