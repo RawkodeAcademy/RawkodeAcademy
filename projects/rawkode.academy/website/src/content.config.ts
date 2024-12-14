a6d4711315edd832fa84ecc82f07dadf373e49c3
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-12-14 01:54:12 +0100

    feat(website): cover images

--- projects/rawkode.academy/website/src/content.config.ts
+++ projects/rawkode.academy/website/src/content.config.ts
@@ -1,20 +1,27 @@
 import { glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
 
+// HINT: image() is described here -> https://docs.astro.build/en/guides/images/#images-in-content-collections
+
 const blog = defineCollection({
   loader: glob({
     pattern: ["**\/*.mdx"],
     base: "./src/content/blog",
   }),
-  schema: z.object({
-    title: z.string(),
-    series: reference("series").optional(),
-    subtitle: z.string().optional(),
-    publishedAt: z.coerce.date(),
-    updatedAt: z.coerce.date().optional(),
-    isDraft: z.boolean().default(true),
-    authors: z.array(z.string()).default(["Rawkode Academy"]),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      cover: z.object({
+        image: image(),
+        alt: z.string(),
+      }).optional(),
+      series: reference("series").optional(),
+      subtitle: z.string().optional(),
+      publishedAt: z.coerce.date(),
+      updatedAt: z.coerce.date().optional(),
+      isDraft: z.boolean().default(true),
+      authors: z.array(z.string()).default(["Rawkode Academy"]),
+    }),
 });
 
 const series = defineCollection({
@@ -22,9 +29,14 @@ const series = defineCollection({
     pattern: ["**\/*.mdx"],
     base: "./src/content/series",
   }),
-  schema: z.object({
-    title: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      cover: z.object({
+        image: image(),
+        alt: z.string(),
+      }).optional(),
+    }),
 });
 
 export const collections = {
