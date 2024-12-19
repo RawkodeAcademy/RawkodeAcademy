e5cb919a7e9653944d78fbc0e2e0618768f5e4b8
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-12-19 11:49:50 +0100

    feat(rawkode.studio): get roles

--- projects/rawkode.studio/src/lib/zitadel.ts
+++ projects/rawkode.studio/src/lib/zitadel.ts
@@ -1,9 +1,11 @@
-import { ZITADEL_CLIENT_ID, ZITADEL_URL } from "astro:env/server";
+import { decodeJWT } from "@oslojs/jwt";
 import type { OAuth2Tokens } from "arctic";
 import { createS256CodeChallenge } from "arctic/dist/oauth2";
 import { createOAuth2Request, sendTokenRequest } from "arctic/dist/request";
-import type { OidcStandardClaims } from "oidc-client-ts";
-import { decodeJWT } from "@oslojs/jwt";
+import { ZITADEL_CLIENT_ID, ZITADEL_URL } from "astro:env/server";
+import type { OidcStandardClaimsWithRoles, Roles } from "./security";
+
+type ZitadelRoles = { [role: string]: { [aud: string]: string } };
 
 export class Zitadel {
   private readonly baseUrl = ZITADEL_URL;
@@ -15,17 +17,24 @@ export class Zitadel {
   private readonly redirectUri = `${import.meta.env.SITE}/api/auth/callback`;
   private readonly tokenEndpoint = `${this.baseUrl}/oauth/v2/token`;
 
+  private readonly scopes = [
+    "openid",
+    "email",
+    "profile",
+    "urn:zitadel:iam:org:project:id:zitadel:aud",
+    "urn:zitadel:iam:org:projects:roles",
+  ];
+
   public createAuthorizationURL(
     state: string,
     codeVerifier: string,
-    scopes: string[],
   ): URL {
     const url = new URL(this.authorizationEndpoint);
 
     url.searchParams.set("response_type", "code");
     url.searchParams.set("client_id", this.clientId);
     url.searchParams.set("state", state);
-    url.searchParams.set("scope", scopes.join(" "));
+    url.searchParams.set("scope", this.scopes.join(" "));
 
     const codeChallenge = createS256CodeChallenge(codeVerifier);
     url.searchParams.set("code_challenge_method", "S256");
@@ -45,6 +54,10 @@ export class Zitadel {
     searchParams.set("code_verifier", codeVerifier);
     searchParams.set("redirect_uri", this.redirectUri);
     searchParams.set("client_id", this.clientId);
+    searchParams.set(
+      "scope",
+      this.scopes.join(" "),
+    );
 
     const request = createOAuth2Request(this.tokenEndpoint, searchParams);
     const tokens = await sendTokenRequest(request);
@@ -56,18 +69,30 @@ export class Zitadel {
     _accessToken: string,
     idToken: string | undefined,
     _refreshToken: string | undefined,
-  ): Promise<OidcStandardClaims | undefined> {
+  ): Promise<OidcStandardClaimsWithRoles | undefined> {
     if (!idToken) {
       return undefined;
     }
 
     try {
-      return decodeJWT(idToken) as OidcStandardClaims;
+      const decodedIdToken = decodeJWT(idToken);
+
+      if ("urn:zitadel:iam:org:project:roles" in decodedIdToken) {
+        const roles =
+          decodedIdToken["urn:zitadel:iam:org:project:roles"] as ZitadelRoles;
+
+        return {
+          ...decodedIdToken,
+          roles: Object.keys(roles).map((role) => role as Roles),
+        };
+      }
+
+      return undefined;
     } catch (e) {
       console.error(e);
     }
 
     // TODO: Eventually add refresh logic
-    return;
+    return undefined;
   }
 }
