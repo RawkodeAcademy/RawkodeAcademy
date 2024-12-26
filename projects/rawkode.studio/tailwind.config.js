057304acd4983fb18148ee26bae1f36774a13dbe
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-12-26 22:54:32 +0100

    bump(rawkode.studio): dependencies

--- projects/rawkode.studio/tailwind.config.js
+++ projects/rawkode.studio/tailwind.config.js
@@ -29,6 +29,8 @@ export default {
           "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
           border: "hsl(var(--sidebar-border))",
           ring: "hsl(var(--sidebar-ring))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
         },
       },
       borderRadius: {
