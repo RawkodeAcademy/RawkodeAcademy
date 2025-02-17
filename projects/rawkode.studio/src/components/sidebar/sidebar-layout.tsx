396a8171472fe1442f7014f5beee0b96811514fb
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2025-02-17 01:14:19 +0100

    chore(rawkode.studio): update some libs & fix theming
    - sidequest: add a mode (dark & light & system) - toggle

--- projects/rawkode.studio/src/components/sidebar/sidebar-layout.tsx
+++ projects/rawkode.studio/src/components/sidebar/sidebar-layout.tsx
@@ -37,6 +37,7 @@ import {
 import { Link } from "react-router";
 import CreateLivestreamsDialog from "../livestreams/create-livestreams-dialog";
 import LivestreamCounter from "./livestream-counter";
+import { ModeToggle } from "../common/ModeToggle";
 
 interface Props {
   title: string;
@@ -145,14 +146,19 @@ export default function SidebarLayout({ user, title, children }: Props) {
 
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                {title}
-              </h4>
+            <div className="flex justify-between w-full pr-4">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  {title}
+                </h4>
+              </div>
+
+              <ModeToggle />
             </div>
           </header>
+
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             {children}
           </div>
