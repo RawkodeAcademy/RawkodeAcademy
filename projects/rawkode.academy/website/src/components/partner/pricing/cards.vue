ad077de3d15d370be234212bdf1c08f657fb482e
David Flanagan <david@rawkode.dev>
2025-02-26 10:36:17 +0000

    chore(website): improve styling of pricing block

--- projects/rawkode.academy/website/src/components/partner/pricing/cards.vue
+++ projects/rawkode.academy/website/src/components/partner/pricing/cards.vue
@@ -40,51 +40,32 @@ const pricingPlans: PricingPlan[] = [
 </script>
 
 <template>
-	<section class="bg-white dark:bg-gray-900">
-		<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-			<div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-				<h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Partnership Plans</h2>
-				<p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">It doesn't matter if you're here to
-					support our mission or because you need help with go to market, product market fit, developer marketing, or
-					straight up content; we have plans for all use-cases.</p>
+	<div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+		<div v-for="plan in pricingPlans"
+			class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white transition-all duration-300 hover:shadow-xl hover:drop-shadow-xl hover:scale-[1.02] hover:border-secondary dark:hover:border-secondary">
+			<h3 class="mb-4 text-2xl font-semibold">{{ plan.title }}</h3>
+			<p class="min-h-32 font-light text-gray-500 sm:text-lg dark:text-gray-400">{{
+				plan.description }}</p>
+			<div class="grid justify-center my-8">
+				<div class="text-5xl font-extrabold">{{ plan.price }}</div>
+				<div class="text-gray-500 dark:text-gray-400">USD / annually</div>
 			</div>
-
-			<div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-				<p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">The Rawkode Academy is proudly brought
-					to
-					you from Glasgow, Scotland. As such, our plans reflect a little bit of our heritage and culture.</p>
-				<p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">🏴󠁧󠁢󠁳󠁣󠁴󠁿 🥃</p>
-			</div>
-
-			<div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-				<div v-for="plan in pricingPlans"
-					class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-					<h3 class="mb-4 text-2xl font-semibold">{{ plan.title }}</h3>
-					<p class="min-h-32 font-light text-gray-500 sm:text-lg dark:text-gray-400">{{
-						plan.description }}</p>
-					<div class="grid justify-center my-8">
-						<div class="text-5xl font-extrabold">{{ plan.price }}</div>
-						<div class="text-gray-500 dark:text-gray-400">USD / annually</div>
-					</div>
-					<!-- List -->
-					<ul role="list" class="mb-8 space-y-4 text-left min-h-64">
-						<li v-for="feature in plan.features" class="flex items-center space-x-3">
-							<!-- Icon -->
-							<svg class="min-h-8 flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
-								viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-								<path fill-rule="evenodd"
-									d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-									clip-rule="evenodd"></path>
-							</svg>
-							<span>{{ feature }}</span>
-						</li>
-					</ul>
-					<a href="/organizations/lets-chat"
-						class="text-white bg-primary hover:bg-secondary focus:ring-4 focus:secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:secondary">Let's
-						Collaborate</a>
-				</div>
-			</div>
-
+			<!-- List -->
+			<ul role="list" class="mb-8 space-y-4 text-left min-h-64">
+				<li v-for="feature in plan.features" class="flex items-center space-x-3">
+					<!-- Icon -->
+					<svg class="min-h-8 flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
+						viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd"
+							d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+							clip-rule="evenodd"></path>
+					</svg>
+					<span>{{ feature }}</span>
+				</li>
+			</ul>
+			<a href="/organizations/lets-chat"
+				class="text-white bg-primary hover:bg-secondary focus:ring-4 focus:secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:secondary">Let's
+				Collaborate</a>
 		</div>
-	</section>
+	</div>
 </template>
--- projects/rawkode.academy/website/src/components/partner/pricing/featured.vue
+++ projects/rawkode.academy/website/src/components/partner/pricing/featured.vue
@@ -20,38 +20,34 @@ const maltPricingPlan: PricingPlan = {
 </script>
 
 <template>
-	<section class="bg-white dark:bg-gray-900">
-		<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-			<div class="border-4 border-secondary bg-white rounded-lg shadow lg:grid lg:grid-cols-3 dark:bg-gray-800">
-				<div class="col-span-2 p-6 lg:p-8">
-					<h2 class="mb-1 text-2xl font-bold text-gray-900 dark:text-white">{{ maltPricingPlan.title }}</h2>
-					<p class="text-lg font-light text-gray-500 dark:text-gray-400">{{ maltPricingPlan.description }}</p>
-					<div class="grid gap-4 mt-4 lg:mt-6">
-						<!-- List -->
-						<ul role="list" class="space-y-4 dark:text-white">
-							<li v-for="feature in maltPricingPlan.features" class="flex space-x-2.5">
-								<!-- Icon -->
-								<svg class="flex-shrink-0 w-5 h-5 text-secondary" fill="currentColor" viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg">
-									<path fill-rule="evenodd"
-										d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-										clip-rule="evenodd"></path>
-								</svg>
-								<span class="leading-tight text-gray-500 dark:text-gray-400">{{ feature }}</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="flex p-6 text-center bg-gray-50 lg:p-8 dark:bg-gray-700">
-					<div class="self-center w-full">
-						<div class="text-5xl font-extrabold text-gray-900 dark:text-white">{{ maltPricingPlan.price }}</div>
-						<div class="mt-1 mb-4 text-gray-500 text-light dark:text-gray-400">USD / anually</div>
-						<a href="/organizations/lets-chat"
-							class="flex justify-center text-white bg-secondary hover:bg-primary focus:ring-4 focus:ring-bue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Let's
-							Excel Together</a>
-					</div>
-				</div>
+	<div class="my-4 mb-12 border-4 border-secondary bg-white rounded-lg shadow lg:grid lg:grid-cols-3 dark:bg-gray-800 transition-all duration-300 hover:shadow-2xl hover:drop-shadow-2xl hover:scale-[1.02]">
+		<div class="col-span-2 p-6 lg:p-8">
+			<h2 class="mb-1 text-2xl font-bold text-gray-900 dark:text-white">{{ maltPricingPlan.title }}</h2>
+			<p class="text-lg font-light text-gray-500 dark:text-gray-400">{{ maltPricingPlan.description }}</p>
+			<div class="grid gap-4 mt-4 lg:mt-6">
+				<!-- List -->
+				<ul role="list" class="space-y-4 dark:text-white">
+					<li v-for="feature in maltPricingPlan.features" class="flex space-x-2.5">
+						<!-- Icon -->
+						<svg class="flex-shrink-0 w-5 h-5 text-secondary" fill="currentColor" viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg">
+							<path fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+								clip-rule="evenodd"></path>
+						</svg>
+						<span class="leading-tight text-gray-500 dark:text-gray-400">{{ feature }}</span>
+					</li>
+				</ul>
 			</div>
 		</div>
-	</section>
+		<div class="flex p-6 text-center bg-gray-50 lg:p-8 dark:bg-gray-700">
+			<div class="self-center w-full">
+				<div class="text-5xl font-extrabold text-gray-900 dark:text-white">{{ maltPricingPlan.price }}</div>
+				<div class="mt-1 mb-4 text-gray-500 text-light dark:text-gray-400">USD / anually</div>
+				<a href="/organizations/lets-chat"
+					class="flex justify-center text-white bg-secondary hover:bg-primary focus:ring-4 focus:ring-bue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Let's
+					Excel Together</a>
+			</div>
+		</div>
+	</div>
 </template>
