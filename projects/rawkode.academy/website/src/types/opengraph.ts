f99a426310968355cca56bcb3d74f86767b7c74c
David Flanagan <david@rawkode.dev>
2025-03-25 16:43:27 +0000

    chore(rawkode.academy/website): improve seo

--- projects/rawkode.academy/website/src/types/opengraph.ts
+++ projects/rawkode.academy/website/src/types/opengraph.ts
@@ -3,11 +3,11 @@ import type { CollectionEntry } from "astro:content";
 
 export interface OpenGraphProps {
   title: string;
-  description?: string;
-  useImageDirectly?: boolean;
-  image?: Partial<ImageServicePayload>;
-  isArticle?: boolean;
-  publishedAt?: Date;
-  updatedAt?: Date;
-  authors?: CollectionEntry<"people">[];
+  description?: string | undefined;
+  useImageDirectly?: boolean | undefined;
+  image?: Partial<ImageServicePayload> | undefined;
+  isArticle?: boolean | undefined;
+  publishedAt?: Date | undefined;
+  updatedAt?: Date | undefined;
+  authors?: CollectionEntry<"people">[] | undefined;
 }
