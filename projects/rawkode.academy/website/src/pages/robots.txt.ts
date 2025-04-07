6f921f29be844b1322997d14b05d018a0e55a472
David Flanagan <david@rawkode.dev>
2025-04-07 11:08:02 +0100

    chore(rawkode.academy/website): trying to stop crawlers hitting server-islands

--- projects/rawkode.academy/website/src/pages/robots.txt.ts
+++ projects/rawkode.academy/website/src/pages/robots.txt.ts
@@ -1,24 +1,28 @@
 import type { APIRoute } from "astro";
 
 const getRobotsTxt = (sitemapURL: URL) => `
+# Host directive to specify canonical domain
+Host: rawkode.academy
+
 # Allow all crawlers
 User-agent: *
 Allow: /
+
+# Except for
 Disallow: /api/
 Disallow: /admin/
 Disallow: /private/
+Disallow: /_server-islands/
 
 # Crawl delay for all bots
 Crawl-delay: 10
 
 # Sitemap location
 Sitemap: ${sitemapURL.href}
-
-# Host directive to specify canonical domain
-Host: rawkode.academy
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap-index.xml", site);
+	const sitemapURL = new URL("sitemap-index.xml", site);
+
   return new Response(getRobotsTxt(sitemapURL));
 };
