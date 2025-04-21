f7fce87bf18fe83b763306ccd6289625b2c77c2f
David Flanagan <david@rawkode.dev>
2025-04-21 14:39:46 +0100

    feat(rawkode.chat/standup-bot): redeploy to Deno Deploy

--- projects/rawkode.chat/standup-bot/main.ts
+++ projects/rawkode.chat/standup-bot/main.ts
@@ -6,16 +6,28 @@ const zulipConfig = {
 	realm: Deno.env.get("ZULIP_REALM"),
 };
 
-// Daily
-Deno.cron("standup-create", "0 3 * * *", async () => {
+const schedule: Deno.CronSchedule = {
+	hour: { exact: 3 }, // 3 AM UTC
+	minute: { exact: 0 }, // 0 minutes past the hour
+	month: { every: 1 }, // Every month
+
+	dayOfMonth: { every: 1 },
+
+	// https://docs.deno.com/deploy/kv/manual/cron/#day-of-week-numeric-representation
+	dayOfWeek: { exact: [2, 3, 4, 5, 6] }, // Monday to Friday
+};
+
+// Run daily, Monday to Friday, at 3 AM UTC
+Deno.cron("standup-create", schedule, async () => {
 	const zulip = await createZulipClient(zulipConfig);
 
-	const formattedDate = new Date().toISOString().split('T')[0];
+	const formattedDate = new Date().toISOString().split("T")[0];
 
 	await zulip.messages.send({
 		to: "Standup",
 		type: "stream",
 		subject: formattedDate,
-		content: "Let's **ship one thing every day**. Share your plan, progress, and problems.",
+		content:
+			"Let's **ship one thing every day**. Share your plan, progress, and problems.",
 	});
 });
