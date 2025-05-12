d13e8f27ed5afd2bdd20012a6c426ea6e594e373
David Flanagan <david@rawkode.dev>
2025-05-12 12:41:19 +0100

    chore(dns): revert records to real production values

--- infrastructure/dns/src/domains/rawkode.com.ts
+++ infrastructure/dns/src/domains/rawkode.com.ts
@@ -7,7 +7,10 @@ export default (scope: Construct): ManagedDomain => {
 		"rawkode.com",
 		Registrar.Cloudflare,
 	)
-		.enableFastmail()
+		.enableGSuite({
+			domainKey:
+				"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoQzFkFcXxJ89NL4omfGZoTszjKVyABVC/sqa3LYHbk2v3G+0xaXyoGdI3CItOcjwuqmnDjyghgxlK961wmpKHLYVnQXIuZbhiY4uXzFcIISgnlxo7Ww5PX4nIGRjnZGZ/vDENfCFPqC1N4thtiMhQBY8kQuEoyKm2ZHa8nBw7cxROL/R6LW8fUwKxt2CJjqEcC4FzT5tR1+U6L3i0Yi1eh5vbvMJbFA45RAGD+RZtmERbOuLQeiJADZqXyjVrTV8OuGwKj5K5N4ua7jCNFjpjCNrTzpsyaOIYAUnXvnbQv9t3ushCtGJkpyHWw41lzRr/yofaP6YwpXi4yNhXkuzdQIDAQAB",
+		})
 		.addTextRecord(
 			"google-site-verification",
 			"@",
--- infrastructure/dns/src/domains/rawkode.dev.ts
+++ infrastructure/dns/src/domains/rawkode.dev.ts
@@ -7,7 +7,10 @@ export default (scope: Construct): ManagedDomain => {
 		"rawkode.dev",
 		Registrar.Cloudflare,
 	)
-		.enableFastmail()
+		.enableGSuite({
+			domainKey:
+				"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvslTU5QuSpPOgpjONa9PsrpzmD4Mm61fFZwyy+ZJ/WcIXEYASwzOrd7SXrxkWrQfb48X8nyB8WBd21xbxx9NOHsHELfoDnKOWe9APj2T3f6Arcg3SEbh03R+4OXTJrTLe9VudB4TjHWGzOcVDeIQDpmnWzAl+oRwQeLGzUII2+cPY0yJGhMAP2cB/Me4II+eCAg5jBMr3QGvCPoUW/9laFiPDJ6T+7ctCUjTVoULDDIug+Jeu/1ES7Bgz5QBLE1Manp7o8FYU8ASQr9RxGtNPq1EEy5RL2fXrJ2On7rcWG6M5eV5EvjG4kPJuxKNGgHaiZhDNhjb1pLf7L1Gtm87+wIDAQAB",
+		})
 		.addTextRecord(
 			"google-site-verification",
 			"@",
@@ -17,7 +20,8 @@ export default (scope: Construct): ManagedDomain => {
 			"_atproto",
 			"_atproto",
 			"did=did:plc:35bdlgus7hihmup66o265nuy",
-		);
+		)
+		.addARecord("gitlab", "@", "51.159.221.54");
 
 	return managedDomain;
 };
