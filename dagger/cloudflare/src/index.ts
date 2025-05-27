3a787b1daceb33e610ad075694b92c54b881833f
David Flanagan <david@rawkode.academy>
2025-05-27 14:26:55 +0100

    fix(cicd): use clouflare version uploads for preview

--- dagger/cloudflare/src/index.ts
+++ dagger/cloudflare/src/index.ts
@@ -1,71 +1,112 @@
 import {
-  dag,
+	dag,
 	Directory,
 	File,
-  func,
-  object,
-  Secret,
+	func,
+	object,
+	Secret,
 } from "@dagger.io/dagger";
 
 @object()
 export class Cloudflare {
-  /**
-   * Deploy a website to Cloudflare Pages.
-   */
-  @func()
-  async deploy(
+	/**
+	 * Deploy a website to Cloudflare Workers.
+	 */
+	@func()
+	async deploy(
 		dist: Directory,
 		wranglerConfig: File,
-    cloudflareApiToken: Secret,
-    gitlabApiToken: Secret,
-    mergeRequestId?: string,
-  ): Promise<string> {
+		cloudflareApiToken: Secret,
+	): Promise<string> {
 		const cloudflareAccountId = await dag.config().cloudflareAccountId();
 
 		const wranglerFilename = await wranglerConfig.name();
 
-    const deploymentResult = await dag.container()
-      .from("node:22")
-      .withWorkdir("/deploy")
-      // Only doing this to cache the wrangler installation
-      .withExec([
-        "npx",
-        "wrangler",
-        "--version",
-      ])
-      .withMountedDirectory("/deploy/dist", dist)
-      .withMountedFile(
-        `/deploy/${wranglerFilename}`,
-        wranglerConfig,
-      )
-      .withEnvVariable("CLOUDFLARE_ACCOUNT_ID", cloudflareAccountId)
-      .withSecretVariable("CLOUDFLARE_API_TOKEN", cloudflareApiToken)
-      .withExec([
-        "npx",
-        "wrangler",
-        "deploy",
-        `--env=${mergeRequestId ? "preview" : "produdction"}`,
-      ]);
+		const deploymentResult = await dag.container()
+			.from("node:22")
+			.withWorkdir("/deploy")
+			// Only doing this to cache the wrangler installation
+			.withExec([
+				"npx",
+				"wrangler",
+				"--version",
+			])
+			.withMountedDirectory("/deploy/dist", dist)
+			.withMountedFile(
+				`/deploy/${wranglerFilename}`,
+				wranglerConfig,
+			)
+			.withEnvVariable("CLOUDFLARE_ACCOUNT_ID", cloudflareAccountId)
+			.withSecretVariable("CLOUDFLARE_API_TOKEN", cloudflareApiToken)
+			.withExec([
+				"npx",
+				"wrangler",
+				"deploy",
+			]);
 
-    if (await deploymentResult.exitCode() !== 0) {
-      throw new Error(
-        "Deployment failed. Error: " + await deploymentResult.stdout() +
-          await deploymentResult.stderr(),
-      );
+		if (await deploymentResult.exitCode() !== 0) {
+			throw new Error(
+				"Deployment failed. Error: " + await deploymentResult.stdout() +
+				await deploymentResult.stderr(),
+			);
 		}
 
-		// remove any line that doesn't start with ✨
+		return deploymentResult.stdout();
+	}
+
+	/**
+	 * Deploy a preview website to Cloudflare Workers.
+	 */
+	@func()
+	async preview(
+		dist: Directory,
+		wranglerConfig: File,
+		cloudflareApiToken: Secret,
+		gitlabApiToken: Secret,
+		mergeRequestId: string,
+	): Promise<string> {
+		const cloudflareAccountId = await dag.config().cloudflareAccountId();
+
+		const wranglerFilename = await wranglerConfig.name();
+
+		const deploymentResult = await dag.container()
+			.from("node:22")
+			.withWorkdir("/deploy")
+			// Only doing this to cache the wrangler installation
+			.withExec([
+				"npx",
+				"wrangler",
+				"--version",
+			])
+			.withMountedDirectory("/deploy/dist", dist)
+			.withMountedFile(
+				`/deploy/${wranglerFilename}`,
+				wranglerConfig,
+			)
+			.withEnvVariable("CLOUDFLARE_ACCOUNT_ID", cloudflareAccountId)
+			.withSecretVariable("CLOUDFLARE_API_TOKEN", cloudflareApiToken)
+			.withExec([
+				"npx",
+				"wrangler",
+				"versions",
+				"upload",
+			]);
+
+		if (await deploymentResult.exitCode() !== 0) {
+			throw new Error(
+				"Deployment failed. Error: " + await deploymentResult.stdout() +
+				await deploymentResult.stderr(),
+			);
+		}
+
 		const allOutput = await deploymentResult.stdout();
-		const goodOutput = allOutput.split("\n").filter((line) => line.startsWith("✨")).join("\n");
 
-    if (mergeRequestId) {
-      return dag.gitlab().postMergeRequestComment(
-        gitlabApiToken,
-        mergeRequestId,
-        goodOutput,
-      ).stdout();
-    }
+		return dag.gitlab().postMergeRequestComment(
+			gitlabApiToken,
+			mergeRequestId,
+			allOutput,
+		).stdout();
 
-    return allOutput;
-  }
+		return allOutput;
+	}
 }
