6551c79fbc68a0064ed9f706c1d47e5cba8e7f72
David Flanagan <david@rawkode.academy>
2025-06-26 13:17:46 +0100

    chore(rawkode.academy/platform/transcription-terms): migrate to Cloudflare with D1

--- projects/rawkode.academy/platform/transcription-terms/drizzle.config.ts
+++ projects/rawkode.academy/platform/transcription-terms/drizzle.config.ts
@@ -1,10 +1,13 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default ({
-  schema: "./data-model/schema.ts",
-  out: "./data-model/migrations",
-  dialect: "turso",
-  breakpoints: true,
-  strict: true,
-  verbose: true,
-} satisfies Config);
+export default defineConfig({
+	out: "../data-model/migrations",
+	schema: "../data-model/schema.ts",
+	dialect: "sqlite",
+	driver: "d1-http",
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		token: process.env.CLOUDFLARE_D1_TOKEN!,
+	},
+});
--- projects/rawkode.academy/platform/transcription-terms/read-model/schema.ts
+++ projects/rawkode.academy/platform/transcription-terms/read-model/schema.ts
@@ -3,57 +3,68 @@ import directivesPlugin from "@pothos/plugin-directives";
 import drizzlePlugin from "@pothos/plugin-drizzle";
 import federationPlugin from "@pothos/plugin-federation";
 import { eq } from "drizzle-orm";
-import { type GraphQLSchema } from "graphql";
-import { db } from "../data-model/client.ts";
-import * as dataSchema from "../data-model/schema.ts";
+import { drizzle } from "drizzle-orm/d1";
+import type { GraphQLSchema } from "graphql";
+import * as dataSchema from "../data-model/schema";
 
 export interface PothosTypes {
-  DrizzleSchema: typeof dataSchema;
+		DrizzleSchema: typeof dataSchema;
+	}
+
+export interface Env {
+	DB: D1Database;
 }
 
-const builder = new schemaBuilder<PothosTypes>({
-  plugins: [directivesPlugin, drizzlePlugin, federationPlugin],
-  drizzle: {
-    client: db,
-  },
-});
+const buildSchema = (env: Env) => {
+	const db = drizzle(env.DB, { schema: dataSchema });
 
-interface Term {
-  term: string;
-}
+	const builder = new schemaBuilder<PothosTypes>({
+		plugins: [directivesPlugin, drizzlePlugin, federationPlugin],
+		drizzle: {
+			client: db,
+		},
+	});
 
-const termRef = builder.objectRef<Term>("Term").implement({
-  fields: (t) => ({
-    term: t.exposeString("term", {
-      nullable: false,
-    }),
-  }),
-});
+	interface Term {
+		term: string;
+	}
 
-builder.externalRef(
-  "Technology",
-  builder.selection<{ id: string }>("id"),
-).implement({
-  externalFields: (t) => ({
-    id: t.string(),
-  }),
-  fields: (t) => ({
-    terms: t.field({
-      type: [termRef],
-      resolve: (technology) =>
-        db.query.transcriptionTerms.findMany({
-          columns: {
-            term: true,
-          },
-          where: eq(dataSchema.transcriptionTerms.foreignId, technology.id),
-        }),
-    }),
-  }),
-});
+	const termRef = builder.objectRef<Term>("Term").implement({
+		fields: (t) => ({
+			term: t.exposeString("term", {
+				nullable: false,
+			}),
+		}),
+	});
 
-export const getSchema = (): GraphQLSchema => {
-  return builder.toSubGraphSchema({
-    linkUrl: "https://specs.apollo.dev/federation/v2.6",
-    federationDirectives: ["@extends", "@external", "@key"],
-  });
+	builder
+		.externalRef("Technology", builder.selection<{ id: string }>("id"))
+		.implement({
+			externalFields: (t) => ({
+				id: t.string(),
+			}),
+			fields: (t) => ({
+				terms: t.field({
+					type: [termRef],
+					resolve: (technology) =>
+						db.query.transcriptionTerms.findMany({
+							columns: {
+								term: true,
+							},
+							where: eq(dataSchema.transcriptionTerms.foreignId, technology.id),
+						}),
+				}),
+			}),
+		});
+
+	return builder;
+};
+
+export const getSchema = (env: Env): GraphQLSchema => {
+	const builder = buildSchema(env);
+
+	return builder.toSubGraphSchema({
+		linkUrl: "https://specs.apollo.dev/federation/v2.6",
+		federationDirectives: ["@extends", "@external", "@key"],
+	});
 };
