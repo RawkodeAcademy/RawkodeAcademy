3872f8a574f46aae08412e94ec601174d60713c8
David Flanagan <david@rawkode.academy>
2025-06-26 22:51:07 +0100

    chore(rawkode.academy/platform/video\s): migrate videos projen

---
+++ projects/rawkode.academy/platform/videos/.projenrc.ts
@@ -0,0 +1,10 @@
+import { PlatformService } from '../../generators/projen-platform-service/src/';
+
+
+const project = new PlatformService({
+  serviceName: 'videos',
+	databaseId: "0e96020b-eb03-456c-a593-fa73f2e845d4",
+	includeWriteModel: false,
+});
+
+project.synth();
