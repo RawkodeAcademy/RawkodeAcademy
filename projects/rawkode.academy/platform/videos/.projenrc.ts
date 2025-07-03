848afa27617928e8eb738fcb7d584f7fb2068d87
David Flanagan <david@rawkode.dev>
2025-07-03 18:16:43 +0100

    fix(rawkode.academy/platform/videos): do not run tests, there are none

--- projects/rawkode.academy/platform/videos/.projenrc.ts
+++ projects/rawkode.academy/platform/videos/.projenrc.ts
@@ -1,10 +1,11 @@
-import { PlatformService } from '../../generators/projen-platform-service/src/';
-
+import { PlatformService } from '../../generators/projen-platform-service/src';
 
 const project = new PlatformService({
   serviceName: 'videos',
 	databaseId: "0e96020b-eb03-456c-a593-fa73f2e845d4",
 	includeWriteModel: false,
+	includeTests: false,
 });
 
 project.synth();
+
