847bfc025634f3387fb7082bff539010573aa707
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2025-07-21 20:50:46 +0200

    cleanup(rawkode.studio): rawkode.studio 2.0 clean slate

--- projects/rawkode.studio/src/components/livestreams/room/layouts/types.ts
+++
@@ -1,7 +0,0 @@
-import type { TrackReferenceOrPlaceholder } from "@livekit/components-react";
-
-export interface LayoutProps {
-  cameraTrackReferences: TrackReferenceOrPlaceholder[];
-  screenShareTrack?: TrackReferenceOrPlaceholder;
-  presenter?: string;
-}
