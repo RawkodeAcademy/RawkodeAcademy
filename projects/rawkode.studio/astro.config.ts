30dfd9f72a4243074dfd43189afb5071d92fe032
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2025-07-21 21:01:21 +0200

    fix(rawkode.studio): deployment

--- projects/rawkode.studio/astro.config.ts
+++ projects/rawkode.studio/astro.config.ts
@@ -1,5 +1,36 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+
+const site = (): string => {
+  if (import.meta.env.CF_PAGES_URL) {
+    return import.meta.env.CF_PAGES_URL;
+  }
+
+  if (import.meta.env.DEV) {
+    return "http://localhost:4321";
+  }
+
+  return "https://rawkode.studio";
+};
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devToolbar: {
+        enabled: false,
+    },
+
+    output: "server",
+
+    adapter: cloudflare({
+        imageService: "cloudflare",
+    }),
+
+    site: site(),
+
+    security: {
+        checkOrigin: true,
+    },
+});
